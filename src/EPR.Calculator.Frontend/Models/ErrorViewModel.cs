7afb539740a86c08d2563288f116b25705eb0b29
johnbenjamin-defra <john.2.benjamin.external@eviden.com>
2024-05-29 14:06:15 +0100

    Initial commit

---
+++ src/EPR.Calculator.Frontend/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EPR.Calculator.Frontend.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
