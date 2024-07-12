0f5a7177d46644c88ad719f70ef09f5ad7904699
johnbenjamin-defra <john.2.benjamin.external@eviden.com>
2024-07-12 14:18:31 +0100

    408840 Upload csv error page

---
+++ src/EPR.Calculator.Frontend/Controllers/UploadCSVErrorController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EPR.Calculator.Frontend.Controllers
+{
+    public class UploadCSVErrorController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
