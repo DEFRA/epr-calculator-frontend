a1078231e98196a1f9e3c40d56cc4d6440052695
Rekha Devi <rekha.devi.external@eviden.com>
2024-07-19 15:59:16 +0100

    Navigation link is added to the confirmation page buttons

--- src/EPR.Calculator.Frontend/Controllers/DashboardController.cs
+++ src/EPR.Calculator.Frontend/Controllers/DashboardController.cs
@@ -6,9 +6,9 @@ namespace EPR.Calculator.Frontend.Controllers
 {
     public class DashboardController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        private readonly ILogger<DashboardController> _logger;
 
-        public DashboardController(ILogger<HomeController> logger)
+        public DashboardController(ILogger<DashboardController> logger)
         {
             _logger = logger;
         }
