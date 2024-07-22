2873a31eff19cef408ed75ef8eec629915ae43df
johnbenjamin-defra <john.2.benjamin.external@eviden.com>
2024-07-22 15:17:15 +0100

    Update Program.cs file

--- src/EPR.Calculator.Frontend/Program.cs
+++ src/EPR.Calculator.Frontend/Program.cs
@@ -17,11 +17,6 @@ if (!app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
-//app.UseStaticFiles(new StaticFileOptions()
-//{
-//    FileProvider = new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory(), @"node_modules")),
-//    RequestPath = new PathString("/node_modules"),
-//});
 
 app.UseRouting();
 
