85f0626a7e5c908e35f55612ffbe36e07f9e874c
Mazar Shaik <mazar.shaik.external@eviden.com>
2024-07-22 12:10:43 +0100

    Reverted test method to see it fix build issue

---
+++ src/EPR.Calculator.Frontend.UnitTests/UnitTest1.cs
@@ -0,0 +1,11 @@
+﻿namespace EPR.Calculator.Frontend.UnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
