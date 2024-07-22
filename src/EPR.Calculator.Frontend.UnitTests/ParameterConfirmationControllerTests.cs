67c2107e9d3169d96d3833b0a60a661aacdcc719
Rekha Devi <rekha.devi.external@eviden.com>
2024-07-22 09:48:25 +0100

    Added the unit test for Dashboard and confirmation page and small chnage for test and style

---
+++ src/EPR.Calculator.Frontend.UnitTests/ParameterConfirmationControllerTests.cs
@@ -0,0 +1,18 @@
+﻿using EPR.Calculator.Frontend.Controllers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EPR.Calculator.Frontend.UnitTests
+{
+    public class ParameterConfirmationControllerTests
+    {
+        [TestMethod]
+        public void ParameterConfirmationController_View_Test()
+        {
+            var controller = new ParameterConfirmationController();
+            var result = controller.Index() as ViewResult;
+            Assert.IsNotNull(result);
+            Assert.AreEqual("Index", result.ViewName);
+
+        }
+    }
+}
--- src/EPR.Calculator.Frontend/Controllers/DashboardController.cs
+++ src/EPR.Calculator.Frontend/Controllers/DashboardController.cs
@@ -6,27 +6,9 @@ namespace EPR.Calculator.Frontend.Controllers
 {
     public class DashboardController : Controller
     {
-        private readonly ILogger<DashboardController> _logger;
-
-        public DashboardController(ILogger<DashboardController> logger)
-        {
-            _logger = logger;
-        }
-
         public IActionResult Index()
         {
-            return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            return View("Index");
         }
     }
 }
