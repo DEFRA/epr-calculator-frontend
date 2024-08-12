101a0e508636e8ffbe3ee3808afb82dbc7614d3e
johnbenjamin-defra <john.2.benjamin.external@eviden.com>
2024-08-12 08:00:40 +0100

    419981-Handle-default-parameter-when-none-exist-for-the-year

--- src/EPR.Calculator.Frontend.UnitTests/DefaultParameterControllerTest.cs
+++ src/EPR.Calculator.Frontend.UnitTests/DefaultParameterControllerTest.cs
@@ -73,7 +73,14 @@ namespace EPR.Calculator.Frontend.UnitTests
                 });
 
             var httpClient = new HttpClient(mockHttpMessageHandler.Object);
-            var controller = new DefaultParametersController(GetConfigurationValues(), httpClient);
+
+            // Mock IHttpClientFactory
+            var mockHttpClientFactory = new Mock<IHttpClientFactory>();
+            mockHttpClientFactory
+                .Setup(_ => _.CreateClient(It.IsAny<string>()))
+                .Returns(httpClient);
+
+            var controller = new DefaultParametersController(GetConfigurationValues(), mockHttpClientFactory.Object);
 
             var result = await controller.Index() as ViewResult;
             Assert.IsNotNull(result);
