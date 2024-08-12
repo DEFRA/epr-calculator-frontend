47d650a3f5138d38fef7be45bda2178ec1e1dd8b
Santhosh Kumar <skumar.hpa@gmail.com>
2024-08-12 11:44:42 +0100

    fixed build issues and merge latest

--- src/EPR.Calculator.Frontend/Controllers/UploadFileController.cs
+++ src/EPR.Calculator.Frontend/Controllers/UploadFileController.cs
@@ -1,11 +1,13 @@
-﻿using CsvHelper;
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.Reflection;
+using System.Text.RegularExpressions;
+using CsvHelper;
 using CsvHelper.Configuration;
 using EPR.Calculator.Frontend.Constants;
 using EPR.Calculator.Frontend.Models;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
-using System.Globalization;
-using System.Text.RegularExpressions;
 
 namespace EPR.Calculator.Frontend.Controllers
 {
