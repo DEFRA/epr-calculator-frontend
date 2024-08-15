25190a5dc9b609393f6bb84f8800f4b4853d2b76
Santhosh Kumar <skumar.hpa@gmail.com>
2024-08-15 11:48:09 +0100

    Implemented unit tests for CSV Helpers

---
+++ src/EPR.Calculator.Frontend.UnitTests/HelpersTest/CsvFileHelperTest.cs
@@ -0,0 +1,73 @@
+﻿using System.Text;
+using EPR.Calculator.Frontend.Constants;
+using EPR.Calculator.Frontend.Helpers;
+using EPR.Calculator.Frontend.UnitTests.Mocks;
+using Microsoft.AspNetCore.Http;
+
+namespace EPR.Calculator.Frontend.UnitTests.HelpersTest
+{
+    [TestClass]
+    public class CsvFileHelperTest
+    {
+        [TestMethod]
+        public void CsvFileHelperTest_Validate_CSV_Test()
+        {
+            var content = MockData.GetSchemeParametersFileContent();
+            var stream = new MemoryStream();
+            var writer = new StreamWriter(stream);
+            writer.Write(content);
+            writer.Flush();
+            stream.Position = 0;
+            IFormFile file = new FormFile(stream, 0, stream.Length, string.Empty, "SchemeParameters.csv");
+
+            var test = CsvFileHelper.ValidateCSV(file);
+            Assert.IsNotNull(test);
+        }
+
+        [TestMethod]
+        public void CsvFileHelperTest_Upload_No_File_Test()
+        {
+            var content = MockData.GetSchemeParametersFileContent();
+            var stream = new MemoryStream();
+            var writer = new StreamWriter(stream);
+            writer.Write(content);
+            writer.Flush();
+            stream.Position = 0;
+            IFormFile file = new FormFile(stream, 0, stream.Length, string.Empty, "SchemeParameters.csv");
+
+            var result = CsvFileHelper.ValidateCSV(null);
+            Assert.IsNotNull(result);
+            Assert.AreEqual(StaticHelpers.FileNotSelected, result.ErrorMessage);
+        }
+
+        [TestMethod]
+        public void CsvFileHelperTest_Upload_Not_CSV_Test()
+        {
+            var content = MockData.GetSchemeParametersFileContent();
+            var stream = new MemoryStream();
+            var writer = new StreamWriter(stream);
+            writer.Write(content);
+            writer.Flush();
+            stream.Position = 0;
+            IFormFile file = new FormFile(stream, 0, stream.Length, string.Empty, "SchemeParameters.xlsx");
+
+            var result = CsvFileHelper.ValidateCSV(file);
+            Assert.IsNotNull(result);
+            Assert.AreEqual(StaticHelpers.FileMustBeCSV, result.ErrorMessage);
+        }
+
+        [TestMethod]
+        public void CsvFileHelperTest_Prepare_Upload__CSV_Test()
+        {
+            var content = MockData.GetSchemeParametersFileContent();
+            var stream = new MemoryStream(Encoding.ASCII.GetBytes(content));
+            var writer = new StreamWriter(stream);
+            writer.Write(content);
+            stream.Position = 0;
+            IFormFile file = new FormFile(stream, 0, stream.Length, string.Empty, "SchemeParameters.csv");
+
+            var result = CsvFileHelper.PrepareDataForUpload(file);
+            Assert.IsNotNull(result);
+        }
+    }
+}
