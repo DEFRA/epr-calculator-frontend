54cf36050a574a26a116460ad9129df2570426bc
johnbenjamin-defra <john.2.benjamin.external@eviden.com>
2024-09-04 22:55:43 +0100

    426618 Local Authority Disposal Costs Mock Data

---
+++ src/EPR.Calculator.Frontend/Models/LocalAuthorityDisposalCost.cs
@@ -0,0 +1,27 @@
+﻿namespace EPR.Calculator.Frontend.Models
+{
+    public class LocalAuthorityDisposalCost
+    {
+        public int Id { get; set; }
+
+        public string ParameterYear { get; set; }
+
+        public DateTime EffectiveFrom { get; set; }
+
+        public DateTime? EffectiveTo { get; set; }
+
+        public string CreatedBy { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+        public int LapcapDataMasterId { get; set; }
+
+        public string LapcapDataTemplateMasterUniqueRef { get; set; }
+
+        public string Country { get; set; }
+
+        public string Material { get; set; }
+
+        public decimal TotalCost { get; set; }
+    }
+}
---
+++ src/EPR.Calculator.Frontend/ViewModels/LocalAuthorityViewModel.cs
@@ -0,0 +1,31 @@
+﻿using EPR.Calculator.Frontend.Models;
+
+namespace EPR.Calculator.Frontend.ViewModels
+{
+    public class LocalAuthorityViewModel
+    {
+        public LocalAuthorityViewModel(LocalAuthorityDisposalCost localAuthorityDisposalCost)
+        {
+            Country = localAuthorityDisposalCost.Country;
+            Material = localAuthorityDisposalCost.Material;
+            TotalCost = localAuthorityDisposalCost.TotalCost;
+            CreatedBy = localAuthorityDisposalCost.CreatedBy;
+            CreatedAt = GetFormattedCreatedAt(localAuthorityDisposalCost.CreatedAt);
+        }
+
+        public string Country { get; set; }
+
+        public string Material { get; set; }
+
+        public decimal TotalCost { get; set; }
+
+        public string CreatedBy { get; set; }
+
+        public string CreatedAt { get; set; }
+
+        private string GetFormattedCreatedAt(DateTime createdAt)
+        {
+            return createdAt.ToString("dd MMM yyyy ' at 'H:mm", new System.Globalization.CultureInfo("en-GB"));
+        }
+    }
+}
