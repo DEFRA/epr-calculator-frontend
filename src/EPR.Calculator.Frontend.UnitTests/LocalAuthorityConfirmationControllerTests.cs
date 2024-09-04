fab1457e15aa1a24cb38a3cca5d026694b51e0fe
Rekha Devi <rekha.devi.external@eviden.com>
2024-09-04 14:26:47 +0100

    unit test cases added for the LAPCAP controller index view

---
+++ src/EPR.Calculator.Frontend.UnitTests/LocalAuthorityConfirmationControllerTests.cs
@@ -0,0 +1,18 @@
+﻿using EPR.Calculator.Frontend.Constants;
+using EPR.Calculator.Frontend.Controllers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EPR.Calculator.Frontend.UnitTests
+{
+    public class LocalAuthorityConfirmationControllerTests
+    {
+        [TestMethod]
+        public void LocalAuthorityConfirmationController_View_Test()
+        {
+            var controller = new LocalAuthorityConfirmationController();
+            var result = controller.Index() as ViewResult;
+            Assert.IsNotNull(result);
+            Assert.AreEqual(ViewNames.LocalAuthorityConfirmationIndex, result.ViewName);
+        }
+    }
+}
