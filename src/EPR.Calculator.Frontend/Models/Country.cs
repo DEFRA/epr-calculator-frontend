f40e94be11c66fbf020ac09626696c652546cd61
johnbenjamin-defra <john.2.benjamin.external@eviden.com>
2024-09-06 16:00:55 +0100

    426618 local authority disposal costs mock data (#41)
    * 426618 Local Authority Disposal Costs Mock Data
    
    * 426618 Local Authority Disposal Costs Mock Data
    
    * implemented no lapcap data message
    
    * 426618 Local Authority Disposal Costs Mock Data
    
    * fixed format issues
    
    * add a comment
    
    * 426618 Local Authority Disposal Costs Mock Data
    
    * 426618 Local Authority Disposal Costs Mock Data
    
    * 426618 Local Authority Disposal Costs Mock Data
    
    * 426618 Local Authority Disposal Costs Mock Data
    
    * 426618 Local Authority Disposal Costs Mock Data
    
    * 426618 Local Authority Disposal Costs Mock Data
    
    * 426618 Local Authority Disposal Costs Mock Data
    
    * 426618 Local Authority Disposal Costs Mock Data
    
    * 426618 Local Authority Disposal Costs Mock Data
    
    * 426618 Local Authority Disposal Costs Mock Data
    
    * 426618 Local Authority Disposal Costs Mock Data
    
    * 426618 Local Authority Disposal Costs Mock Data
    
    ---------
    
    Co-authored-by: Santhosh Kumar <skumar.hpa@gmail.com>

---
+++ src/EPR.Calculator.Frontend/Models/Country.cs
@@ -0,0 +1,22 @@
+﻿using System.Runtime.Serialization;
+
+namespace EPR.Calculator.Frontend.Models
+{
+    public enum Country
+    {
+        [EnumMember(Value = "United Kingdom")]
+        UnitedKingdom = 0,
+
+        [EnumMember(Value = "England")]
+        England = 1,
+
+        [EnumMember(Value = "Wales")]
+        Wales = 2,
+
+        [EnumMember(Value = "Scotland")]
+        Scotland = 3,
+
+        [EnumMember(Value = "Northern Ireland")]
+        NI = 4,
+    }
+}
---
+++ src/EPR.Calculator.Frontend/Models/LocalAuthorityDisposalCost.cs
@@ -0,0 +1,30 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace EPR.Calculator.Frontend.Models
+{
+    [ExcludeFromCodeCoverage]
+    public class LocalAuthorityDisposalCost
+    {
+        public int Id { get; set; }
+
+        public required string ParameterYear { get; set; }
+
+        public required DateTime EffectiveFrom { get; set; }
+
+        public DateTime? EffectiveTo { get; set; }
+
+        public required string CreatedBy { get; set; }
+
+        public required DateTime CreatedAt { get; set; }
+
+        public required int LapcapDataMasterId { get; set; }
+
+        public required string LapcapDataTemplateMasterUniqueRef { get; set; }
+
+        public required string Country { get; set; }
+
+        public required string Material { get; set; }
+
+        public required decimal TotalCost { get; set; }
+    }
+}
