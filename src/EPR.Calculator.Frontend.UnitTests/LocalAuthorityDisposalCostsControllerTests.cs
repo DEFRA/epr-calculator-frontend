6f0fa0e93564926caee6ef16a97723e8252a13f4
Rekha Devi <rekha.devi.external@eviden.com>
2024-09-17 11:40:16 +0100

    build time error fixes

--- src/EPR.Calculator.Frontend.UnitTests/LocalAuthorityDisposalCostsControllerTests.cs
+++ src/EPR.Calculator.Frontend.UnitTests/LocalAuthorityDisposalCostsControllerTests.cs
@@ -1,19 +1,126 @@
-﻿using EPR.Calculator.Frontend.Constants;
+﻿using System.Net;
+using EPR.Calculator.Frontend.Constants;
 using EPR.Calculator.Frontend.Controllers;
+using EPR.Calculator.Frontend.UnitTests.Mocks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Moq;
+using Moq.Protected;
+using Newtonsoft.Json;
 
 namespace EPR.Calculator.Frontend.UnitTests
 {
     [TestClass]
     public class LocalAuthorityDisposalCostsControllerTests
     {
+        private static readonly string[] BinSeparator = new string[] { @"bin\" };
+
         [TestMethod]
-        public void LocalAuthorityDisposalCostsController_View_Test()
+        public async Task LocalAuthorityDisposalCostsController_Success_View_Test()
         {
-            var controller = new LocalAuthorityDisposalCostsController();
+            var mockHttpMessageHandler = new Mock<HttpMessageHandler>();
+            mockHttpMessageHandler
+                   .Protected()
+                .Setup<Task<HttpResponseMessage>>(
+                    "SendAsync",
+                    ItExpr.IsAny<HttpRequestMessage>(),
+                    ItExpr.IsAny<CancellationToken>())
+                .ReturnsAsync(new HttpResponseMessage
+                {
+                    StatusCode = HttpStatusCode.OK,
+                    Content = new StringContent(JsonConvert.SerializeObject(MockData.GetLocalAuthorityDisposalCosts()))
+                });
+
+            var httpClient = new HttpClient(mockHttpMessageHandler.Object);
+
+            // Mock IHttpClientFactory
+            var mockHttpClientFactory = new Mock<IHttpClientFactory>();
+            mockHttpClientFactory
+                .Setup(_ => _.CreateClient(It.IsAny<string>()))
+                .Returns(httpClient);
+
+            var controller = new LocalAuthorityDisposalCostsController(GetConfigurationValues(), mockHttpClientFactory.Object);
+
             var result = controller.Index() as ViewResult;
             Assert.IsNotNull(result);
-            Assert.AreEqual(ViewNames.LocalAuthorityDisposalCostsIndex, result.ViewName);
+        }
+
+        [TestMethod]
+        public async Task LocalAuthorityDisposalCostsController_Success_No_Data_View_Test()
+        {
+            var content = "No data available for the specified year.Please check the year and try again.";
+            var mockHttpMessageHandler = new Mock<HttpMessageHandler>();
+            mockHttpMessageHandler
+                   .Protected()
+                .Setup<Task<HttpResponseMessage>>(
+                    "SendAsync",
+                    ItExpr.IsAny<HttpRequestMessage>(),
+                    ItExpr.IsAny<CancellationToken>())
+                .ReturnsAsync(new HttpResponseMessage
+                {
+                    StatusCode = HttpStatusCode.NotFound,
+                    Content = new StringContent(content)
+                });
+
+            var httpClient = new HttpClient(mockHttpMessageHandler.Object);
+
+            // Mock IHttpClientFactory
+            var mockHttpClientFactory = new Mock<IHttpClientFactory>();
+            mockHttpClientFactory
+                .Setup(_ => _.CreateClient(It.IsAny<string>()))
+                .Returns(httpClient);
+
+            var controller = new LocalAuthorityDisposalCostsController(GetConfigurationValues(), mockHttpClientFactory.Object);
+
+            var result = controller.Index() as ViewResult;
+            Assert.IsNotNull(result);
+            Assert.AreEqual(false, result.ViewData["IsDataAvailable"]);
+        }
+
+        [TestMethod]
+        public async Task LocalAuthorityDisposalCostsController_Failure_View_Test()
+        {
+            var mockHttpMessageHandler = new Mock<HttpMessageHandler>();
+            mockHttpMessageHandler
+                   .Protected()
+                .Setup<Task<HttpResponseMessage>>(
+                    "SendAsync",
+                    ItExpr.IsAny<HttpRequestMessage>(),
+                    ItExpr.IsAny<CancellationToken>())
+                .ReturnsAsync(new HttpResponseMessage
+                {
+                    StatusCode = HttpStatusCode.BadRequest,
+                    Content = new StringContent("Sample content")
+                });
+
+            var httpClient = new HttpClient(mockHttpMessageHandler.Object);
+            // Mock IHttpClientFactory
+            var mockHttpClientFactory = new Mock<IHttpClientFactory>();
+            mockHttpClientFactory
+                .Setup(_ => _.CreateClient(It.IsAny<string>()))
+                .Returns(httpClient);
+            var controller = new LocalAuthorityDisposalCostsController(GetConfigurationValues(), mockHttpClientFactory.Object);
+
+            var result = controller.Index() as RedirectToActionResult;
+            Assert.IsNotNull(result);
+            Assert.AreEqual(ActionNames.StandardErrorIndex, result.ActionName);
+            Assert.AreEqual("StandardError", result.ControllerName);
+        }
+
+        private static void NewMethod(IActionResult result)
+        {
+            Assert.IsInstanceOfType(result, typeof(RedirectToActionResult));
+        }
+
+        private static IConfiguration GetConfigurationValues()
+        {
+            string projectPath = AppDomain.CurrentDomain.BaseDirectory.Split(BinSeparator, StringSplitOptions.None)[0];
+            IConfiguration config = new ConfigurationBuilder()
+               .SetBasePath(projectPath)
+               .AddJsonFile("appsettings.Test.json")
+               .Build();
+
+            return config;
         }
     }
 }
