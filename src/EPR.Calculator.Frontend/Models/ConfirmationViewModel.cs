db294712f2559bef50c0525e4bd98accad9bcf11
Santhosh <111443660+Skumarhpa@users.noreply.github.com>
2024-09-27 14:48:28 +0100

    Feature/433035 parameter maintenace update final parameters (#52)
    * updated test data file
    
    * updated the scheme parameter template
    
    * inlcude communications costs by nation
    
    * updated the parameter types and UI
    
    * updated the template and respective code
    
    * Implemented confirmation partial
    
    * addressed review comments
    
    * updated constant
    
    * updated the constants
    
    * Build fix after merge latest
    
    * updated test

---
+++ src/EPR.Calculator.Frontend/Models/ConfirmationViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace EPR.Calculator.Frontend.Models
+{
+    [ExcludeFromCodeCoverage]
+    public class ConfirmationViewModel
+    {
+        public string Title { get; set; }
+
+        public string SubmitText { get; set; }
+
+        public string NextText { get; set; }
+
+        public string RedirectController { get; set; }
+
+        public string Body { get; set; }
+    }
+}
