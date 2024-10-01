1b6e8c04c4c4b82c1a5c4d55df9af4cef42af6e3
Mazar-Shaik <mazar.shaik.external@eviden.com>
2024-10-01 11:36:27 +0100

    Calculation Run Name Empty Check (#54)
    * Added code for Check CalculationRun Name Empty
    
    * Added testmethods and SQ fix
    
    * Fix for Code review comments
    
    * Added string interpolation
    
    * Added code for Check CalculationRun Name Empty
    
    * Added testmethods and SQ fix
    
    * Fix for Code review comments
    
    * Added string interpolation

--- src/EPR.Calculator.Frontend.UnitTests/CalculationRunNameControllerTests.cs
+++ src/EPR.Calculator.Frontend.UnitTests/CalculationRunNameControllerTests.cs
@@ -1,5 +1,6 @@
 ﻿using EPR.Calculator.Frontend.Constants;
 using EPR.Calculator.Frontend.Controllers;
+using EPR.Calculator.Frontend.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace EPR.Calculator.Frontend.UnitTests
@@ -14,6 +15,29 @@ namespace EPR.Calculator.Frontend.UnitTests
             var result = controller.Index() as ViewResult;
             Assert.IsNotNull(result);
             Assert.AreEqual(ViewNames.CalculationRunNameIndex, result.ViewName);
+        }
+
+        [TestMethod]
+        public void RunCalculator_ShouldReturnView_WhenCalculationNameIsInvalid()
+        {
+            var controller = new CalculationRunNameController();
+            var result = controller.RunCalculator(null) as ViewResult;
+            Assert.IsNotNull(result);
+            Assert.AreEqual(ViewNames.CalculationRunNameIndex, result.ViewName);
+            var errorViewModel = controller.ViewBag.Errors as ErrorViewModel;
+            Assert.IsNotNull(errorViewModel);
+            Assert.AreEqual(ViewControlNames.CalculationRunName, errorViewModel.DOMElementId);
+            Assert.AreEqual(ErrorMessages.CalculationRunNameEmpty, errorViewModel.ErrorMessage);
+        }
+
+        [TestMethod]
+        public void RunCalculator_ShouldRedirect_WhenCalculationNameIsValid()
+        {
+            var controller = new CalculationRunNameController();
+            var result = controller.RunCalculator("ValidCalculationName") as RedirectToActionResult;
+            Assert.IsNotNull(result);
+            Assert.AreEqual("Index", result.ActionName);
+            Assert.AreEqual("CalculationRunConfirmation", result.ControllerName);
         }
     }
 }
---
+++ src/EPR.Calculator.Frontend/Constants/ErrorMessages.cs
@@ -0,0 +1,10 @@
+﻿namespace EPR.Calculator.Frontend.Constants
+{
+    public static class ErrorMessages
+    {
+        public const string FileNotSelected = "Please select a file";
+        public const string FileMustBeCSV = "Your file must have the extension .csv";
+        public const string FileNotExceed50KB = "The CSV must be smaller than 50KB";
+        public const string CalculationRunNameEmpty = "Enter a name for this calculation";
+    }
+}
