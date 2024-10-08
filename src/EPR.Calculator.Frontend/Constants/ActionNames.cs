ee00bd3a6cf03f654bba44520768ac646d0d378c
sunitaguptaEviden <sunita.gupta.external@eviden.com>
2024-10-08 10:18:41 +0100

    Build Run Calculator Confirmation Screen (#58)
    * Created CalculationRunConfirmation Page
    
    * Changed IsNullOrEmpty to IsNullOrWhiteSpace
    
    * Added funtionality for not reposting the runCalculate action
    
    * Added Confirmation view test
    
    * Added SessionConstants
    
    * Useded ActionNames Constant in partial view

--- src/EPR.Calculator.Frontend/Constants/ActionNames.cs
+++ src/EPR.Calculator.Frontend/Constants/ActionNames.cs
@@ -3,5 +3,7 @@
     public static class ActionNames
     {
         public const string StandardErrorIndex = "Index";
+        public const string RunCalculatorConfirmation = "Confirmation";
+        public const string Index = "Index";
     }
 }
