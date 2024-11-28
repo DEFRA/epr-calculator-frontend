8b1db563cbbba53fc5761e76d9882ed7d32389e7
Rekha Devi <rekha.devi.external@eviden.com>
2024-11-28 14:20:43 +0000

    Added the controller and views for three screens

---
+++ src/EPR.Calculator.Frontend/Controllers/DeleteRunConfirmedScreenController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EPR.Calculator.Frontend.Controllers
+{
+    public class DeleteRunConfirmedScreenController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ src/EPR.Calculator.Frontend/Controllers/DownloadUnavailableErrorController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EPR.Calculator.Frontend.Controllers
+{
+    public class DownloadUnavailableErrorController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
