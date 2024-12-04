e59a9c3659aa12174b7a2e86088c26ae686ddc6a
Rekha Devi <rekha.devi.external@eviden.com>
2024-12-04 21:36:15 +0000

    buil fixes

--- src/EPR.Calculator.Frontend/ViewModels/CalculatorRunStatusUpdateDto.cs
+++ src/EPR.Calculator.Frontend/ViewModels/CalculatorRunStatusUpdateDto.cs
@@ -5,6 +5,7 @@ namespace EPR.Calculator.Frontend.ViewModels
     public class CalculatorRunStatusUpdateDto
     {
         public int RunId { get; set; }
+
         public int ClassificationId { get; set; }
 
         public string CalcName { get; set; }
