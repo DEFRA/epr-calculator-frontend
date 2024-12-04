65556a22327f817f120f6d3a94b66057458b151c
David Jayakumar <david.2.jayakumar.external@eviden.com>
2024-12-04 16:56:17 +0000

    Add delete confirmation

--- src/EPR.Calculator.Frontend/Controllers/DeleteConfirmationController.cs
+++ src/EPR.Calculator.Frontend/Controllers/DeleteConfirmationController.cs
@@ -1,12 +1,28 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using EPR.Calculator.Frontend.Constants;
+using EPR.Calculator.Frontend.Enums;
+using EPR.Calculator.Frontend.ViewModels;
+using Microsoft.AspNetCore.Mvc;
 
 namespace EPR.Calculator.Frontend.Controllers
 {
+    /// <summary>
+    /// Initializes a new instance of the <see cref="DeleteConfirmationController"/> class.
+    /// </summary>
     public class DeleteConfirmationController : Controller
     {
-        public IActionResult Index()
+        /// <summary>
+        /// Displays the delete confirmation screen.
+        /// </summary>
+        /// <param name="runId">The ID of the calculation run.</param>
+        /// <returns>The delete success view.</returns>
+        public IActionResult Index(int runId)
         {
-            return View();
+            var calculatorRunStatusUpdate = new CalculatorRunStatusUpdateDto
+            {
+                RunId = runId,
+                ClassificationId = (int)RunClassification.DELETED,
+            };
+            return this.View(ViewNames.DeleteConfirmation, calculatorRunStatusUpdate);
         }
     }
 }
