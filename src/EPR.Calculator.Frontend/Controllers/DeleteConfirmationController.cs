943dea77e27f7b18939f8733f9daa38e1f79b0dc
Rekha Devi <rekha.devi.external@eviden.com>
2024-12-05 10:07:36 +0000

    Adding more unit test coverage

--- src/EPR.Calculator.Frontend/Controllers/DeleteConfirmationController.cs
+++ src/EPR.Calculator.Frontend/Controllers/DeleteConfirmationController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using EPR.Calculator.Frontend.Constants;
+using Microsoft.AspNetCore.Mvc;
 
 namespace EPR.Calculator.Frontend.Controllers
 {
@@ -6,7 +7,7 @@ namespace EPR.Calculator.Frontend.Controllers
     {
         public IActionResult Index()
         {
-            return this.View();
+            return this.View(ViewNames.DeleteConfirmation);
         }
     }
 }
