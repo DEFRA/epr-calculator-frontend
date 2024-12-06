30ce1f5f99e0fda9d1127139988df29602ee272b
Mazar Shaik <mazar.shaik.external@eviden.com>
2024-12-06 14:49:16 +0000

    updated test as per session

--- src/EPR.Calculator.Frontend.UnitTests/LocalAuthorityUploadFileControllerTests.cs
+++ src/EPR.Calculator.Frontend.UnitTests/LocalAuthorityUploadFileControllerTests.cs
@@ -24,6 +24,34 @@ namespace EPR.Calculator.Frontend.UnitTests
         }
 
         [TestMethod]
+        public async Task LocalAuthorityUploadFileController_Upload_View_File_Valid_Test()
+        {
+            var httpContext = new DefaultHttpContext();
+            var tempData = new TempDataDictionary(httpContext, Mock.Of<ITempDataProvider>());
+
+            tempData["LapcapFilePath"] = Directory.GetCurrentDirectory() + "/Mocks/LocalAuthorityData.csv";
+            tempData["LapcapFileName"] = "LocalAuthorityData.csv";
+
+            var controller = new LocalAuthorityUploadFileController(_telemetryClient)
+            {
+                TempData = tempData
+            };
+
+            var mockHttpContext = new Mock<HttpContext>();
+            var mockSession = new Mock<ISession>();
+            mockHttpContext.Setup(s => s.Session).Returns(mockSession.Object);
+            controller.ControllerContext = new ControllerContext
+            {
+                HttpContext = mockHttpContext.Object
+            };
+
+            var result = await controller.Upload() as ViewResult;
+            Assert.IsNotNull(result);
+            Assert.AreEqual(ViewNames.LocalAuthorityUploadFileRefresh, result.ViewName);
+            Assert.AreEqual(result.TempData["LapcapFileName"].ToString(), "LocalAuthorityData.csv");
+        }
+
+        [TestMethod]
         public async Task LocalAuthorityUploadFileController_Upload_Incorrect_File_Test()
         {
             var httpContext = new DefaultHttpContext();
@@ -36,6 +64,7 @@ namespace EPR.Calculator.Frontend.UnitTests
                 TempData = tempData
             };
 
+            var mockSession = new Mock<ISession>();
             var result = await controller.Upload() as ViewResult;
             Assert.IsNotNull(result);
             Assert.AreEqual(ViewNames.LocalAuthorityUploadFileIndex, result.ViewName);
@@ -76,6 +105,74 @@ namespace EPR.Calculator.Frontend.UnitTests
             Assert.IsNotNull(result);
             Assert.AreEqual(ActionNames.StandardErrorIndex, result.ActionName);
             Assert.AreEqual("StandardError", result.ControllerName);
+        }
+
+        [TestMethod]
+        public async Task LocalAuthorityUploadFileController_Upload_View_Post_Test()
+        {
+            var content = MockData.GetLocalAuthorityDisposalCosts();
+            var stream = new MemoryStream();
+            var writer = new StreamWriter(stream);
+            writer.Write(content);
+            writer.Flush();
+            stream.Position = 0;
+            var file = new FormFile(stream, 0, stream.Length, string.Empty, "LocalAuthorityData.csv");
+
+            var httpContext = new DefaultHttpContext();
+            var tempData = new TempDataDictionary(httpContext, Mock.Of<ITempDataProvider>());
+            tempData[UploadFileErrorIds.LocalAuthorityUploadErrors] = string.Empty;
+            tempData["LapcapFileName"] = "LocalAuthorityData.csv";
+
+            var controller = new LocalAuthorityUploadFileController(_telemetryClient)
+            {
+                TempData = tempData
+            };
+
+            var mockHttpContext = new Mock<HttpContext>();
+            var mockSession = new Mock<ISession>();
+            mockHttpContext.Setup(s => s.Session).Returns(mockSession.Object);
+            controller.ControllerContext = new ControllerContext
+            {
+                HttpContext = mockHttpContext.Object
+            };
+
+            var result = await controller.Upload(file) as ViewResult;
+            Assert.IsNotNull(result);
+            Assert.AreEqual(ViewNames.LocalAuthorityUploadFileRefresh, result.ViewName);
+        }
+
+        [TestMethod]
+        public async Task LocalAuthorityUploadFileController_Upload_View_Post_Error_Test()
+        {
+            var content = string.Empty;
+            var stream = new MemoryStream();
+            var writer = new StreamWriter(stream);
+            writer.Write(content);
+            writer.Flush();
+            stream.Position = 0;
+            var file = new FormFile(stream, 0, stream.Length, string.Empty, "LocalAuthorityData.csv");
+
+            var httpContext = new DefaultHttpContext();
+            var tempData = new TempDataDictionary(httpContext, Mock.Of<ITempDataProvider>());
+            tempData[UploadFileErrorIds.LocalAuthorityUploadErrors] = string.Empty;
+            tempData["LapcapFileName"] = "LocalAuthorityData.csv";
+
+            var controller = new LocalAuthorityUploadFileController(_telemetryClient)
+            {
+                TempData = tempData
+            };
+
+            var mockHttpContext = new Mock<HttpContext>();
+            var mockSession = new Mock<ISession>();
+            mockHttpContext.Setup(s => s.Session).Returns(mockSession.Object);
+            controller.ControllerContext = new ControllerContext
+            {
+                HttpContext = mockHttpContext.Object
+            };
+
+            var result = await controller.Upload(file) as ViewResult;
+            Assert.IsNotNull(result);
+            Assert.AreEqual(ViewNames.LocalAuthorityUploadFileRefresh, result.ViewName);
         }
 
         [TestMethod]
@@ -126,6 +223,39 @@ namespace EPR.Calculator.Frontend.UnitTests
             var result = await controller.Upload(file) as ViewResult;
             Assert.IsNotNull(result);
             Assert.AreEqual(ViewNames.LocalAuthorityUploadFileIndex, result.ViewName);
+        }
+
+        [TestMethod]
+        public async Task LocalAuthorityUploadFileController_ModelState_Clear_Test()
+        {
+            var httpContext = new DefaultHttpContext();
+            var tempData = new TempDataDictionary(httpContext, Mock.Of<ITempDataProvider>());
+
+            tempData["LapcapFilePath"] = Directory.GetCurrentDirectory() + "/Mocks/LocalAuthorityData.csv";
+            tempData["LapcapFileName"] = "LocalAuthorityData.csv";
+
+            var controller = new LocalAuthorityUploadFileController(_telemetryClient)
+            {
+                TempData = tempData
+            };
+
+            var mockHttpContext = new Mock<HttpContext>();
+            var mockSession = new Mock<ISession>();
+            mockHttpContext.Setup(s => s.Session).Returns(mockSession.Object);
+            controller.ControllerContext = new ControllerContext
+            {
+                HttpContext = mockHttpContext.Object
+            };
+
+            // Arrange
+            controller.ModelState.AddModelError("key", "Paper value is missing");
+
+            // Act
+            Assert.IsFalse(controller.ModelState.IsValid);
+
+            await controller.Upload();
+            // Assert
+            Assert.IsTrue(controller.ModelState.IsValid);
         }
     }
 }
--- src/EPR.Calculator.Frontend.UnitTests/LocalAuthorityUploadFileProcessingControllerTests.cs
+++ src/EPR.Calculator.Frontend.UnitTests/LocalAuthorityUploadFileProcessingControllerTests.cs
@@ -1,4 +1,5 @@
 ﻿using System.Net;
+using System.Text;
 using EPR.Calculator.Frontend.Constants;
 using EPR.Calculator.Frontend.Controllers;
 using EPR.Calculator.Frontend.UnitTests.Mocks;
@@ -9,6 +10,7 @@ using Microsoft.AspNetCore.Mvc.ViewFeatures;
 using Microsoft.Extensions.Configuration;
 using Moq;
 using Moq.Protected;
+using Newtonsoft.Json;
 
 namespace EPR.Calculator.Frontend.UnitTests
 {
@@ -17,6 +19,121 @@ namespace EPR.Calculator.Frontend.UnitTests
     {
         private static readonly string[] Separator = new string[] { @"bin\" };
         private TelemetryClient _telemetryClient = new();
+
+        [TestMethod]
+        public void LocalAuthorityUploadFileProcessingController_Success_Result_Test()
+        {
+            // Mock HttpMessageHandler
+            var mockHttpMessageHandler = new Mock<HttpMessageHandler>();
+            mockHttpMessageHandler
+                .Protected()
+                .Setup<Task<HttpResponseMessage>>(
+                    "SendAsync",
+                    ItExpr.IsAny<HttpRequestMessage>(),
+                    ItExpr.IsAny<CancellationToken>()).ReturnsAsync(new HttpResponseMessage
+                    {
+                        StatusCode = HttpStatusCode.Created,
+                        Content = new StringContent("response content"),
+                    });
+
+            // Create HttpClient with the mocked handler
+            var httpClient = new HttpClient(mockHttpMessageHandler.Object);
+
+            // Mock IHttpClientFactory
+            var mockHttpClientFactory = new Mock<IHttpClientFactory>();
+            mockHttpClientFactory
+                .Setup(_ => _.CreateClient(It.IsAny<string>()))
+                .Returns(httpClient);
+
+            var httpContext = new DefaultHttpContext();
+            var tempData = new TempDataDictionary(httpContext, Mock.Of<ITempDataProvider>());
+            tempData["LapcapFileName"] = "LocalAuthorityData.csv";
+
+            // Create controller with the mocked factory
+            var controller = new LocalAuthorityUploadFileProcessingController(GetConfigurationValues(), mockHttpClientFactory.Object, _telemetryClient)
+            {
+                TempData = tempData
+            };
+
+            // Arrange
+            var fileUploadFileName = "LocalAuthorityData.csv";
+
+            // Mock ISession
+            var sessionMock = new Mock<ISession>();
+            var sessionStorage = new Dictionary<string, byte[]>();
+
+            sessionMock.Setup(s => s.Set(It.IsAny<string>(), It.IsAny<byte[]>()))
+                       .Callback<string, byte[]>((key, value) => sessionStorage[key] = value);
+
+            sessionMock.Setup(s => s.TryGetValue(It.IsAny<string>(), out It.Ref<byte[]>.IsAny))
+                       .Returns((string key, out byte[] value) =>
+                       {
+                           var success = sessionStorage.TryGetValue(key, out var storedValue);
+                           value = storedValue;
+                           return success;
+                       });
+
+            sessionMock.Setup(s => s.Remove(It.IsAny<string>()))
+                       .Callback<string>((key) => sessionStorage.Remove(key));
+
+            var httpContextMock = new Mock<HttpContext>();
+            httpContextMock.Setup(ctx => ctx.Session).Returns(sessionMock.Object);
+            controller.ControllerContext.HttpContext = httpContextMock.Object;
+            controller.HttpContext.Session.SetString("LapcapFileName", fileUploadFileName);
+
+            // Act
+            var result = controller.Index(MockData.GetLocalAuthorityDisposalCostsToUpload().ToList()) as OkObjectResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual(200, result.StatusCode);
+            Assert.AreEqual("LocalAuthorityData.csv", controller.FileName);
+            Assert.IsTrue(sessionStorage.ContainsKey("LapcapFileName"));
+        }
+
+        [TestMethod]
+        public void LocalAuthorityUploadFileProcessingController_Failure_Result_Test()
+        {
+            var mockHttpMessageHandler = new Mock<HttpMessageHandler>();
+            mockHttpMessageHandler
+                .Protected()
+                .Setup<Task<HttpResponseMessage>>(
+                    "SendAsync",
+                    ItExpr.IsAny<HttpRequestMessage>(),
+                    ItExpr.IsAny<CancellationToken>()).ReturnsAsync(new HttpResponseMessage
+                    {
+                        StatusCode = HttpStatusCode.BadRequest,
+                        Content = new StringContent("response content"),
+                    });
+
+            var httpClient = new HttpClient(mockHttpMessageHandler.Object);
+            var mockHttpClientFactory = new Mock<IHttpClientFactory>();
+
+            var httpContext = new DefaultHttpContext();
+            var tempData = new TempDataDictionary(httpContext, Mock.Of<ITempDataProvider>());
+            tempData["LapcapFileName"] = "LocalAuthorityData.csv";
+
+            mockHttpClientFactory
+                .Setup(_ => _.CreateClient(It.IsAny<string>()))
+                    .Returns(httpClient);
+
+            var controller = new LocalAuthorityUploadFileProcessingController(GetConfigurationValues(), mockHttpClientFactory.Object, _telemetryClient)
+            {
+                TempData = tempData
+            };
+
+            var mockHttpContext = new Mock<HttpContext>();
+            var mockSession = new Mock<ISession>();
+            mockHttpContext.Setup(s => s.Session).Returns(mockSession.Object);
+            controller.ControllerContext = new ControllerContext
+            {
+                HttpContext = mockHttpContext.Object
+            };
+
+            var result = controller.Index(MockData.GetLocalAuthorityDisposalCostsToUpload().ToList()) as BadRequestObjectResult;
+            Assert.IsNotNull(result);
+            Assert.AreNotEqual(201, result.StatusCode);
+        }
 
         [TestMethod]
         public void LocalAuthorityUploadFileProcessingController_ArgumentNullExceptionForAPIConfig_Test()
