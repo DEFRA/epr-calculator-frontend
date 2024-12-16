7edae9f5333ec0f03a72d92042b65c361d3aec94
udaydefra <uday.2.denduluri.external@eviden.com>
2024-12-16 18:21:23 +0000

    Just one scope

--- src/EPR.Calculator.Frontend/Controllers/DashboardController.cs
+++ src/EPR.Calculator.Frontend/Controllers/DashboardController.cs
@@ -51,7 +51,6 @@ namespace EPR.Calculator.Frontend.Controllers
         /// Thrown when the API URL is null or empty.
         /// </exception>
         [Authorize(Roles = "SASuperUser")]
-        [AuthorizeForScopes(Scopes = new[] { "api://542488b9-bf70-429f-bad7-1e592efce352/default", "User.Read" })]
         public async Task<IActionResult> Index()
         {
             try
@@ -59,7 +58,7 @@ namespace EPR.Calculator.Frontend.Controllers
                 var accessToken = HttpContext?.Session?.GetString("accessToken");
                 if (string.IsNullOrEmpty(accessToken))
                 {
-                    var scopes = new List<string> { "api://542488b9-bf70-429f-bad7-1e592efce352/Read_Scope", "api://542488b9-bf70-429f-bad7-1e592efce352/Write_Scope" };
+                    var scopes = new List<string> { "api://542488b9-bf70-429f-bad7-1e592efce352/Read_Scope" };
                     accessToken = await this.tokenAcquisition.GetAccessTokenForUserAsync(scopes);
                     this.telemetryClient.TrackTrace("after generating..");
                     HttpContext?.Session?.SetString("accessToken", accessToken);
