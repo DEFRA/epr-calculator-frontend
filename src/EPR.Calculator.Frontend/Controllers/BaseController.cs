dc99a948d6f0a4471cebeafc2a1f0629ad1d3a91
udaydefra <uday.2.denduluri.external@eviden.com>
2025-01-09 16:39:06 +0000

    Adding Scope back

--- src/EPR.Calculator.Frontend/Controllers/BaseController.cs
+++ src/EPR.Calculator.Frontend/Controllers/BaseController.cs
@@ -35,8 +35,18 @@ namespace EPR.Calculator.Frontend.Controllers
             var token = this.HttpContext?.Session?.GetString("accessToken");
             if (string.IsNullOrEmpty(token))
             {
-                var scopeArray = this.Configuration.GetSection("DownstreamApi:Scopes").AsEnumerable();
-                var scope = string.Join(" ", scopeArray.Where(x => x.Value != null).Select(x => x.Value));
+                // var scopeArray = this.Configuration.GetSection("DownstreamApi:Scopes").AsEnumerable();
+                // var scope = string.Join(" ", scopeArray.Where(x => x.Value != null).Select(x => x.Value));
+                var scopeArray = new[]
+                {
+                    "openid",
+                    "offline_access",
+                    "api://542488b9-bf70-429f-bad7-1e592efce352/Read_Scope",
+                    "api://542488b9-bf70-429f-bad7-1e592efce352/Write_Scope",
+                    "api://542488b9-bf70-429f-bad7-1e592efce352/default",
+                };
+
+                var scope = string.Join(" ", scopeArray);
                 this.TelemetryClient.TrackTrace($"scope is {scope}");
                 token = await this.tokenAcquisition.GetAccessTokenForUserAsync([scope]);
                 this.TelemetryClient.TrackTrace("after generating..");
