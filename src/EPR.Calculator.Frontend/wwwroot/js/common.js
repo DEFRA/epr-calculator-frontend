188c96a3c6456d22b419cbde0dd51b150f84492f
Santhosh Kumar <skumar.hpa@gmail.com>
2025-02-17 15:11:04 +0000

    Fixed the resubnission of the form

---
+++ src/EPR.Calculator.Frontend/wwwroot/js/common.js
@@ -0,0 +1,10 @@
+﻿(function () {
+    function preventFormReSubmission(formElement) {
+        if (formElement.dataset.submitted === "true") {
+            return false;
+        }
+        formElement.dataset.submitted = "true";
+        return true;
+    }
+    window.preventFormReSubmission = preventFormReSubmission;
+})();
