a9291b438655742d8ff0c149d6424183642a3819
Santhosh kumar <111443660+Skumarhpa@users.noreply.github.com>
2025-02-25 14:29:29 +0000

    updated the error message (#116)

--- src/EPR.Calculator.Frontend/Constants/ErrorMessages.cs
+++ src/EPR.Calculator.Frontend/Constants/ErrorMessages.cs
@@ -8,7 +8,7 @@
         public const string CalculationRunNameEmpty = "Enter a name for this calculation";
         public const string CalculationRunNameMaxLengthExceeded = "Calculation name must contain no more than 100 characters";
         public const string CalculationRunNameMustBeAlphaNumeric = "Calculation name must only contain numbers and letters";
-        public const string CalculationRunNameExists = "There is already calculation with this name";
+        public const string CalculationRunNameExists = "There is already a calculation with this name";
         public const string SelectDeleteCalculation = "Select delete to continue or return to the dashboard";
         public const string DeleteCalculationError = "This file cannot be deleted";
         public const string UnknownUser = "Unknown User";
