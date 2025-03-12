7fa5e722b5bf47a35057a823ff334cc6386e900d
Santhosh Kumar <skumar.hpa@gmail.com>
2025-03-12 13:11:36 +0000

    Refactored Default parameters

--- src/EPR.Calculator.Frontend/Controllers/DefaultParametersController.cs
+++ src/EPR.Calculator.Frontend/Controllers/DefaultParametersController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Net;
 using Azure.Core;
 using EPR.Calculator.Frontend.Constants;
+using EPR.Calculator.Frontend.Extensions;
 using EPR.Calculator.Frontend.Helpers;
 using EPR.Calculator.Frontend.Models;
 using EPR.Calculator.Frontend.ViewModels;
@@ -66,29 +67,26 @@ namespace EPR.Calculator.Frontend.Controllers
 
                 if (response.IsSuccessStatusCode)
                 {
+                    var viewModel = new DefaultParametersViewModel
+                    {
+                        CurrentUser = CommonUtil.GetUserName(this.HttpContext),
+                        LastUpdatedBy = CommonUtil.GetUserName(this.HttpContext),
+                        SchemeParameters = new List<SchemeParametersViewModel>(),
+                    };
                     var data = await response.Content.ReadAsStringAsync();
                     var defaultSchemeParameters = JsonConvert.DeserializeObject<List<DefaultSchemeParameters>>(data);
 
                     if (defaultSchemeParameters != null)
                     {
-                        this.ViewBag.CommunicationData = GetSchemeParametersBasedonCategory(defaultSchemeParameters, ParameterType.CommunicationCostsByMaterial);
-                        this.ViewBag.CommunicationCostsByCountry = GetSchemeParametersBasedonCategory(defaultSchemeParameters, ParameterType.CommunicationCostsByCountry);
-                        this.ViewBag.OperatingCosts = GetSchemeParametersBasedonCategory(defaultSchemeParameters, ParameterType.SchemeAdministratorOperatingCosts);
-                        this.ViewBag.PreparationCosts = GetSchemeParametersBasedonCategory(defaultSchemeParameters, ParameterType.LocalAuthorityDataPreparationCosts);
-                        this.ViewBag.SchemeSetupCosts = GetSchemeParametersBasedonCategory(defaultSchemeParameters, ParameterType.SchemeSetupCosts);
-                        this.ViewBag.LateReportingTonnage = GetSchemeParametersBasedonCategory(defaultSchemeParameters, ParameterType.LateReportingTonnage);
-                        this.ViewBag.MaterialityThreshold = GetSchemeParametersBasedonCategory(defaultSchemeParameters, ParameterType.MaterialityThreshold);
-                        this.ViewBag.BadDebtProvision = GetSchemeParametersBasedonCategory(defaultSchemeParameters, ParameterType.BadDebtProvision);
-                        this.ViewBag.TonnageChange = GetSchemeParametersBasedonCategory(defaultSchemeParameters, ParameterType.TonnageChangeThreshold);
-                        this.ViewBag.EffectiveFrom = defaultSchemeParameters.First().EffectiveFrom;
-                        this.ViewBag.IsDataAvailable = true;
+                        foreach (ParameterType name in (ParameterType[]) Enum.GetValues(typeof(ParameterType)))
+                        {
+                            viewModel.SchemeParameters.Add(GetSchemeParametersBasedonCategory(defaultSchemeParameters, name.GetDisplayName()));
+                        }
 
-                        return this.View(
-                            new DefaultParametersViewModel
-                            {
-                                CurrentUser = CommonUtil.GetUserName(this.HttpContext),
-                                LastUpdatedBy = defaultSchemeParameters.First().CreatedBy,
-                            });
+                        viewModel.EffectiveFrom = defaultSchemeParameters.First().EffectiveFrom;
+                        viewModel.IsDataAvailable = true;
+
+                        return this.View(viewModel);
                     }
                 }
 
@@ -104,16 +102,20 @@ namespace EPR.Calculator.Frontend.Controllers
 
                 return this.RedirectToAction(ActionNames.StandardErrorIndex, "StandardError");
             }
-            catch (Exception)
+            catch (Exception ex)
             {
                 return this.RedirectToAction(ActionNames.StandardErrorIndex, "StandardError");
             }
         }
 
-        private static List<DefaultSchemeParameters> GetSchemeParametersBasedonCategory(List<DefaultSchemeParameters> defaultSchemeParameters, string type)
+        private static SchemeParametersViewModel GetSchemeParametersBasedonCategory(List<DefaultSchemeParameters> defaultSchemeParameters, string type)
         {
-            var schemeParametersBasedonCategory = defaultSchemeParameters.Where(t => t.ParameterType == type).ToList();
-            return schemeParametersBasedonCategory;
+            return new SchemeParametersViewModel
+            {
+                DefaultSchemeParameters = defaultSchemeParameters.Where(t => t.ParameterType == type).ToList(),
+                IsDisplayPrefix = !(type == nameof(ParameterType.LateReportingTonnage) || type == nameof(ParameterType.BadDebtProvision)),
+                SchemeParameterName = type,
+            };
         }
     }
 }
---
+++ src/EPR.Calculator.Frontend/ViewModels/SchemeParametersViewModel.cs
@@ -0,0 +1,13 @@
+﻿using EPR.Calculator.Frontend.Models;
+
+namespace EPR.Calculator.Frontend.ViewModels
+{
+    public class SchemeParametersViewModel
+    {
+        public IEnumerable<DefaultSchemeParameters> DefaultSchemeParameters { get; set; }
+
+        public string SchemeParameterName {  get; set; }
+
+        public bool IsDisplayPrefix {  get; set; }
+    }
+}
