abaf5abfa45c04a252a1c3bf7fc3a4af86bf3c21
Jignesh Jethwani (ext) <jignesh.jethwani.external@eviden.com>
2025-03-24 12:08:31 +0000

    checking if I can commit the code to repo

--- src/EPR.Calculator.Frontend/Constants/ApiUrl.cs
+++ src/EPR.Calculator.Frontend/Constants/ApiUrl.cs
@@ -2,6 +2,9 @@
 
 namespace EPR.Calculator.Frontend.Constants
 {
+    /// <summary>
+    ///  Api Url.
+    /// </summary>
     public static class ApiUrl
     {
         public const string Url = "The API URL cannot be null or empty.";
