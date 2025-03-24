2c1f31d0326091b133a0775a298e3a66a9f13591
Mazar Shaik <mazar.shaik.external@eviden.com>
2025-03-24 15:52:29 +0000

    Test coverage

---
+++ src/EPR.Calculator.Frontend.UnitTests/ViewModels/LapcapRefreshViewModelTests.cs
@@ -0,0 +1,38 @@
+﻿namespace EPR.Calculator.Frontend.UnitTests.ViewModels
+{
+    using System;
+    using System.Collections.Generic;
+    using AutoFixture;
+    using AutoFixture.AutoMoq;
+    using EPR.Calculator.Frontend.Models;
+    using EPR.Calculator.Frontend.ViewModels;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class LapcapRefreshViewModelTests
+    {
+        private LapcapRefreshViewModel _testClass;
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            var fixture = new Fixture().Customize(new AutoMoqCustomization());
+            _testClass = fixture.Create<LapcapRefreshViewModel>();
+        }
+
+        [TestMethod]
+        public void CanSetAndGetLapcapTemplateValue()
+        {
+            // Arrange
+            var fixture = new Fixture().Customize(new AutoMoqCustomization());
+
+            var testValue = fixture.Create<List<LapcapDataTemplateValueDto>>();
+
+            // Act
+            _testClass.LapcapTemplateValue = testValue;
+
+            // Assert
+            Assert.AreSame(testValue, _testClass.LapcapTemplateValue);
+        }
+    }
+}
---
+++ src/EPR.Calculator.Frontend.UnitTests/ViewModels/LapcapUploadViewModelTests.cs
@@ -0,0 +1,37 @@
+﻿namespace EPR.Calculator.Frontend.UnitTests.ViewModels
+{
+    using System;
+    using AutoFixture;
+    using AutoFixture.AutoMoq;
+    using EPR.Calculator.Frontend.Models;
+    using EPR.Calculator.Frontend.ViewModels;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class LapcapUploadViewModelTests
+    {
+        private LapcapUploadViewModel _testClass;
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            var fixture = new Fixture().Customize(new AutoMoqCustomization());
+            _testClass = fixture.Create<LapcapUploadViewModel>();
+        }
+
+        [TestMethod]
+        public void CanSetAndGetErrors()
+        {
+            // Arrange
+            var fixture = new Fixture().Customize(new AutoMoqCustomization());
+
+            var testValue = fixture.Create<ErrorViewModel>();
+
+            // Act
+            _testClass.Errors = testValue;
+
+            // Assert
+            Assert.AreSame(testValue, _testClass.Errors);
+        }
+    }
+}
