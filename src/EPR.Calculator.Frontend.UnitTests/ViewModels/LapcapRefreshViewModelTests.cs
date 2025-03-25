49ae2a31b962d652bbfbd536c50f54b9a7b6e28a
Tim Powell <timothy.powell@eviden.com>
2025-03-25 11:16:43 +0000

    Add more unit tests

--- src/EPR.Calculator.Frontend.UnitTests/ViewModels/LapcapRefreshViewModelTests.cs
+++ src/EPR.Calculator.Frontend.UnitTests/ViewModels/LapcapRefreshViewModelTests.cs
@@ -1,6 +1,5 @@
 ﻿namespace EPR.Calculator.Frontend.UnitTests.ViewModels
 {
-    using System;
     using System.Collections.Generic;
     using AutoFixture;
     using AutoFixture.AutoMoq;
@@ -11,14 +10,16 @@
     [TestClass]
     public class LapcapRefreshViewModelTests
     {
-        private LapcapRefreshViewModel _testClass;
-
-        [TestInitialize]
-        public void SetUp()
+        public LapcapRefreshViewModelTests()
         {
+            Fixture = new Fixture().Customize(new AutoMoqCustomization());
             var fixture = new Fixture().Customize(new AutoMoqCustomization());
-            _testClass = fixture.Create<LapcapRefreshViewModel>();
+            TestClass = fixture.Create<LapcapRefreshViewModel>();
         }
+
+        private LapcapRefreshViewModel TestClass { get; set; }
+
+        private IFixture Fixture { get; init; }
 
         [TestMethod]
         public void CanSetAndGetLapcapTemplateValue()
@@ -29,10 +30,38 @@
             var testValue = fixture.Create<List<LapcapDataTemplateValueDto>>();
 
             // Act
-            _testClass.LapcapTemplateValue = testValue;
+            TestClass = new LapcapRefreshViewModel
+            {
+                LapcapTemplateValue = testValue,
+            };
 
             // Assert
-            Assert.AreSame(testValue, _testClass.LapcapTemplateValue);
+            Assert.AreSame(testValue, TestClass.LapcapTemplateValue);
+        }
+
+        [TestMethod]
+        public void ImplementsIEquatable_LapcapRefreshViewModel()
+        {
+            // Arrange
+            var same = new LapcapRefreshViewModel
+            {
+                LapcapTemplateValue = TestClass.LapcapTemplateValue,
+            };
+            var different = Fixture.Create<LapcapRefreshViewModel>();
+
+            // Assert
+            Assert.IsFalse(this.TestClass.Equals(default(object)));
+            Assert.IsFalse(this.TestClass.Equals(new object()));
+            Assert.IsTrue(this.TestClass.Equals((object)same));
+            Assert.IsFalse(this.TestClass.Equals((object)different));
+            Assert.IsTrue(this.TestClass.Equals(same));
+            Assert.IsFalse(this.TestClass.Equals(different));
+            Assert.AreEqual(same.GetHashCode(), this.TestClass.GetHashCode());
+            Assert.AreNotEqual(different.GetHashCode(), this.TestClass.GetHashCode());
+            Assert.IsTrue(this.TestClass == same);
+            Assert.IsFalse(this.TestClass == different);
+            Assert.IsFalse(this.TestClass != same);
+            Assert.IsTrue(this.TestClass != different);
         }
     }
 }
--- src/EPR.Calculator.Frontend.UnitTests/ViewModels/LapcapUploadViewModelTests.cs
+++ src/EPR.Calculator.Frontend.UnitTests/ViewModels/LapcapUploadViewModelTests.cs
@@ -1,6 +1,6 @@
 ﻿namespace EPR.Calculator.Frontend.UnitTests.ViewModels
 {
-    using System;
+    using System.Collections.Generic;
     using AutoFixture;
     using AutoFixture.AutoMoq;
     using EPR.Calculator.Frontend.Models;
@@ -10,14 +10,16 @@
     [TestClass]
     public class LapcapUploadViewModelTests
     {
-        private LapcapUploadViewModel _testClass;
-
-        [TestInitialize]
-        public void SetUp()
+        public LapcapUploadViewModelTests()
         {
+            Fixture = new Fixture().Customize(new AutoMoqCustomization());
             var fixture = new Fixture().Customize(new AutoMoqCustomization());
-            _testClass = fixture.Create<LapcapUploadViewModel>();
+            TestClass = fixture.Create<LapcapUploadViewModel>();
         }
+
+        private IFixture Fixture { get; set; }
+
+        private LapcapUploadViewModel TestClass { get; set; }
 
         [TestMethod]
         public void CanSetAndGetErrors()
@@ -28,10 +30,79 @@
             var testValue = fixture.Create<ErrorViewModel>();
 
             // Act
-            _testClass.Errors = testValue;
+            TestClass.Errors = testValue;
 
             // Assert
-            Assert.AreSame(testValue, _testClass.Errors);
+            Assert.AreSame(testValue, TestClass.Errors);
+        }
+
+        [TestMethod]
+        public void ImplementsIEquatable_LapcapUploadViewModel()
+        {
+            // Arrange
+            var same = new LapcapUploadViewModel
+            {
+                CurrentUser = this.TestClass.CurrentUser,
+                Errors = this.TestClass.Errors,
+                LapcapDataTemplateValue = this.TestClass.LapcapDataTemplateValue,
+                LapcapErrors = this.TestClass.LapcapErrors,
+                ValidationErrors = this.TestClass.ValidationErrors,
+            };
+
+            var different = Fixture.Create<LapcapUploadViewModel>();
+
+            // Assert
+            Assert.IsFalse(this.TestClass.Equals(default(object)));
+            Assert.IsFalse(this.TestClass.Equals(new object()));
+            Assert.IsTrue(this.TestClass.Equals((object)same));
+            Assert.IsFalse(this.TestClass.Equals((object)different));
+            Assert.IsTrue(this.TestClass.Equals(same));
+            Assert.IsFalse(this.TestClass.Equals(different));
+            Assert.AreEqual(same.GetHashCode(), this.TestClass.GetHashCode());
+            Assert.AreNotEqual(different.GetHashCode(), this.TestClass.GetHashCode());
+            Assert.IsTrue(this.TestClass == same);
+            Assert.IsFalse(this.TestClass == different);
+            Assert.IsFalse(this.TestClass != same);
+            Assert.IsTrue(this.TestClass != different);
+        }
+
+        [TestMethod]
+        public void CanSetAndGetLapcapErrors()
+        {
+            // Arrange
+            var testValue = Fixture.Create<List<CreateLapcapDataErrorDto>>();
+
+            // Act
+            this.TestClass.LapcapErrors = testValue;
+
+            // Assert
+            Assert.AreSame(testValue, this.TestClass.LapcapErrors);
+        }
+
+        [TestMethod]
+        public void CanSetAndGetValidationErrors()
+        {
+            // Arrange
+            var testValue = Fixture.Create<List<ValidationErrorDto>>();
+
+            // Act
+            this.TestClass.ValidationErrors = testValue;
+
+            // Assert
+            Assert.AreSame(testValue, this.TestClass.ValidationErrors);
+        }
+
+        [TestMethod]
+        public void CanSetAndGetLapcapDataTemplateValue()
+        {
+            // Arrange
+            var testValue = Fixture.Create<List<LapcapDataTemplateValueDto>>();
+
+            // Act
+            this.TestClass.LapcapDataTemplateValue = testValue;
+
+            // Assert
+            Assert.AreSame(testValue, this.TestClass.LapcapDataTemplateValue);
         }
     }
 }
