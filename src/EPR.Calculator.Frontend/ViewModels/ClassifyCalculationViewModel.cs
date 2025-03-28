80017d97ee74d463ae7f308f595e0309007346aa
Rekha Devi <rekha.devi.external@eviden.com>
2025-03-28 10:54:31 +0000

    Resolve Comment from PR

--- src/EPR.Calculator.Frontend/ViewModels/ClassifyCalculationViewModel.cs
+++ src/EPR.Calculator.Frontend/ViewModels/ClassifyCalculationViewModel.cs
@@ -6,7 +6,7 @@
     /// <param name="CalcName">The name of the calculation.</param>
     /// <param name="FinancialYear">The financial year of the calculation.</param>
     /// <param name="CalculationId">The unique identifier for the calculation.</param>
-    /// <param name="ClassificationType">The type of classification (nullable).</param>
-    /// <param name="RunDate">The date the calculation was run (nullable).</param>
-    public record ClassifyCalculationViewModel(string CalcName, string FinancialYear, int CalculationId, bool? ClassificationType, string? RunDate) : ViewModelCommonData;
+    /// <param name="ClassificationType">The type of classification.</param>
+    /// <param name="RunDate">The date the calculation was run.</param>
+    public record ClassifyCalculationViewModel(string CalcName, string FinancialYear, int CalculationId, bool ClassificationType, string RunDate) : ViewModelCommonData;
 }
