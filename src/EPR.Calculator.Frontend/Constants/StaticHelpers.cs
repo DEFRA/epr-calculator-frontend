5c6697590148283638dec1ffce4dcdd24d1f033c
John Jebastin Benjamin <john.2.benjamin.external@eviden.com>
2025-03-31 20:35:35 +0100

    519892, 527298 Feature flag and financial year selection (#127)
    * 519892 Financial year selection
    
    * 519892 Financial year selection
    
    * 519892 Financial year selection
    
    * 519892 Financial year selection
    
    * 519892 Financial year selection
    
    * 519892 Financial year selection
    
    * 519892 Financial year selection
    
    * 519892 Financial year selection
    
    * Update FeatureManagementServiceTests.cs
    
    * Update GlobalSuppressions.cs
    
    * Update EPR.Calculator.Frontend.Common.csproj
    
    * Update Dockerfile
    
    * 519892 Financial year selection
    
    * 519892 Financial year selection
    
    * 519892 Financial year selection
    
    * Update CommonUtilTests.cs
    
    * Update DashboardControllerTests.cs
    
    * Update DashboardControllerTests.cs
    
    * 519892 Financial year selection
    
    * 519892 Financial year selection
    
    * 519892 Financial year selection
    
    * 519892 Financial year selection
    
    * 519892 Financial year selection
    
    * 519892 Financial year selection
    
    * 527298 Feature flag financial year selection
    
    * Update Program.cs
    
    * Ignore the unwanted files

--- src/EPR.Calculator.Frontend/Constants/StaticHelpers.cs
+++ src/EPR.Calculator.Frontend/Constants/StaticHelpers.cs
@@ -1,12 +1,38 @@
 ﻿namespace EPR.Calculator.Frontend.Constants
 {
+    /// <summary>
+    /// List of static helpers.
+    /// </summary>
     public static class StaticHelpers
     {
-        public const string Path = @"wwwroot/assets/SchemeParameterTemplates/SchemeParameterTemplate.v1.1.xlsx";
-        public const string FileName = "test.csv";
-        public const string MimeType = "test/csv";
-        public const long MaxFileSize = 50 * 1024; // 50 KB
-        public const string MediaType = "application/json";
+        /// <summary>
+        /// Static helper for alphanumeric.
+        /// </summary>
         public const string AlphaNumeric = "^[a-zA-Z0-9 ]*$";
+
+        /// <summary>
+        /// Static helper for file name.
+        /// </summary>
+        public const string FileName = "test.csv";
+
+        /// <summary>
+        /// Static helper for maximum file size.
+        /// </summary>
+        public const long MaxFileSize = 50 * 1024; // 50 KB
+
+        /// <summary>
+        /// Static helper for media type.
+        /// </summary>
+        public const string MediaType = "application/json";
+
+        /// <summary>
+        /// Static helper for mime type.
+        /// </summary>
+        public const string MimeType = "test/csv";
+
+        /// <summary>
+        /// Static helper for path.
+        /// </summary>
+        public const string Path = @"wwwroot/assets/SchemeParameterTemplates/SchemeParameterTemplate.v1.1.xlsx";
     }
 }
