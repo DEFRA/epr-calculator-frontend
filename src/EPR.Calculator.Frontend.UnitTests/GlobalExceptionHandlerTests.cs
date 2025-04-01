c795b01bd021d86ea5a170432a14a153a0eb62b5
Jignesh Jethwani (ext) <jignesh.jethwani.external@eviden.com>
2025-04-01 12:53:12 +0100

    Updated GlobalHandlerTests to include Development and Local environment

--- src/EPR.Calculator.Frontend.UnitTests/GlobalExceptionHandlerTests.cs
+++ src/EPR.Calculator.Frontend.UnitTests/GlobalExceptionHandlerTests.cs
@@ -56,9 +56,11 @@ namespace EPR.Calculator.Frontend.UnitTests
         }
 
         [TestMethod]
-        public async Task TryHandleAsync_ReturnsExpectedJsonResponse_InLocal()
+        [DataRow("Development")]
+        [DataRow("local")]
+        public async Task TryHandleAsync_ReturnsExpectedJsonResponse_InDevelopment_Or_InLocal(string environmentName)
         {
-            _mockEnv.Setup(env => env.EnvironmentName).Returns("local");
+            _mockEnv.Setup(env => env.EnvironmentName).Returns(environmentName);
             var exception = new Exception("Test exception");
 
             await _exceptionHandler.TryHandleAsync(_httpContext, exception, _cancellationToken);
@@ -73,45 +75,11 @@ namespace EPR.Calculator.Frontend.UnitTests
         }
 
         [TestMethod]
-        public async Task TryHandleAsync_ReturnsExpectedJsonResponse_InDevelopment()
+        [DataRow("Development")]
+        [DataRow("local")]
+        public async Task TryHandleAsync_ReturnsExpectedJsonResponse_DummyStackTrace_InDevelopment_Or_InLocal(string environmentName)
         {
-            _mockEnv.Setup(env => env.EnvironmentName).Returns(Environments.Development);
-            var exception = new Exception("Test exception");
-
-            await _exceptionHandler.TryHandleAsync(_httpContext, exception, _cancellationToken);
-
-            _httpContext.Response.Body.Seek(0, SeekOrigin.Begin);
-            var jsonResponse = await new StreamReader(_httpContext.Response.Body).ReadToEndAsync();
-            var responseObject = JsonSerializer.Deserialize<JsonElement>(jsonResponse);
-
-            Assert.AreEqual(StatusCodes.Status500InternalServerError, responseObject.GetProperty("Status").GetInt32());
-            Assert.AreEqual("An error occurred while processing your request.", responseObject.GetProperty("Title").GetString());
-            Assert.AreEqual(exception.Message, responseObject.GetProperty("Message").GetString());
-        }
-
-        [TestMethod]
-        public async Task TryHandleAsync_ReturnsExpectedJsonResponse_DummyStackTrace_InDevelopment()
-        {
-            _mockEnv.Setup(env => env.EnvironmentName).Returns(Environments.Development);
-            var dummyStackTrace = "at DummyNamespace.DummyClass.DummyMethod() in /DummyFile.cs:line 42";
-            var exception = GlobalExceptionHandlerTestsHelpers.CreateExceptionWithDummyStackTrace("Test exception with dummy stack trace", dummyStackTrace);
-
-            await _exceptionHandler.TryHandleAsync(_httpContext, exception, _cancellationToken);
-
-            _httpContext.Response.Body.Seek(0, SeekOrigin.Begin);
-            var jsonResponse = await new StreamReader(_httpContext.Response.Body).ReadToEndAsync();
-            var responseObject = JsonSerializer.Deserialize<JsonElement>(jsonResponse);
-
-            Assert.AreEqual(StatusCodes.Status500InternalServerError, responseObject.GetProperty("Status").GetInt32());
-            Assert.AreEqual("An error occurred while processing your request.", responseObject.GetProperty("Title").GetString());
-            Assert.AreEqual(exception.Message, responseObject.GetProperty("Message").GetString());
-            Assert.AreEqual(exception.StackTrace, responseObject.GetProperty("Detail").GetString());
-        }
-
-        [TestMethod]
-        public async Task TryHandleAsync_ReturnsExpectedJsonResponse_DummyStackTrace_InLocal()
-        {
-            _mockEnv.Setup(env => env.EnvironmentName).Returns("local");
+            _mockEnv.Setup(env => env.EnvironmentName).Returns(environmentName);
             var dummyStackTrace = "at DummyNamespace.DummyClass.DummyMethod() in /DummyFile.cs:line 42";
             var exception = GlobalExceptionHandlerTestsHelpers.CreateExceptionWithDummyStackTrace("Test exception with dummy stack trace", dummyStackTrace);
 
