4eb975a81cef231234ab7713ef1817693b879e20
Shilpi Goel <shilpi.goel.external@eviden.com>
2025-04-02 13:41:07 +0100

    Added Unit tests

---
+++ src/EPR.Calculator.Frontend.UnitTests/CalculationRunOverviewControllerTests.cs
@@ -0,0 +1,66 @@
+﻿using EPR.Calculator.Frontend.Controllers;
+using EPR.Calculator.Frontend.UnitTests.HelpersTest;
+using EPR.Calculator.Frontend.ViewModels;
+using Microsoft.ApplicationInsights;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Microsoft.Identity.Web;
+using Moq;
+
+namespace EPR.Calculator.Frontend.UnitTests
+{
+    [TestClass]
+    public class CalculationRunOverviewControllerTests
+    {
+        private readonly IConfiguration _configuration = ConfigurationItems.GetConfigurationValues();
+        private Mock<IHttpClientFactory> _mockHttpClientFactory;
+        private Mock<ILogger<CalculationRunOverviewController>> _mockLogger;
+        private Mock<ITokenAcquisition> _mockTokenAcquisition;
+        private TelemetryClient _telemetryClient;
+        private CalculationRunOverviewController _controller;
+        private Mock<HttpContext> _mockHttpContext;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _mockHttpClientFactory = new Mock<IHttpClientFactory>();
+            _mockLogger = new Mock<ILogger<CalculationRunOverviewController>>();
+            _mockTokenAcquisition = new Mock<ITokenAcquisition>();
+            _telemetryClient = new TelemetryClient();
+            _mockHttpContext = new Mock<HttpContext>();
+
+            _controller = new CalculationRunOverviewController(
+                   _configuration,
+                   _mockHttpClientFactory.Object,
+                   _mockLogger.Object,
+                   _mockTokenAcquisition.Object,
+                   _telemetryClient)
+            {
+                // Setting the mocked HttpContext for the controller
+                ControllerContext = new ControllerContext
+                {
+                    HttpContext = _mockHttpContext.Object
+                }
+            };
+        }
+
+        [TestMethod]
+        public async Task IndexAsync_ReturnsViewResult_WithValidViewModel()
+        {
+            // Arrange
+            int testRunId = 1;
+
+            // Mocking HttpContext.User.Identity.Name to simulate a logged-in user
+            _mockHttpContext.Setup(ctx => ctx.User.Identity.Name).Returns("TestUser");
+
+            var result = await _controller.IndexAsync(testRunId);
+
+            Assert.IsInstanceOfType(result, typeof(ViewResult));
+            var viewResult = result as ViewResult;
+            Assert.IsNotNull(viewResult);
+            Assert.IsInstanceOfType(viewResult.Model, typeof(CalculatorRunOverviewViewModel));
+        }
+    }
+}
--- src/EPR.Calculator.Frontend/Controllers/CalculationRunOverviewController.cs
+++ src/EPR.Calculator.Frontend/Controllers/CalculationRunOverviewController.cs
@@ -1,6 +1,5 @@
 ﻿using EPR.Calculator.Frontend.Common.Constants;
 using EPR.Calculator.Frontend.Constants;
-using EPR.Calculator.Frontend.Enums;
 using EPR.Calculator.Frontend.Helpers;
 using EPR.Calculator.Frontend.Models;
 using EPR.Calculator.Frontend.ViewModels;
@@ -8,7 +7,6 @@ using Microsoft.ApplicationInsights;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Identity.Web;
-using Newtonsoft.Json;
 
 namespace EPR.Calculator.Frontend.Controllers
 {
@@ -19,17 +17,18 @@ namespace EPR.Calculator.Frontend.Controllers
     [Route("payment-calculator")]
     public class CalculationRunOverviewController : BaseController
     {
-        private readonly IHttpClientFactory _clientFactory;
-        private readonly ILogger<CalculationRunOverviewController> _logger;
         private readonly IConfiguration _configuration;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="CalculationRunOverviewController"/> class.
         /// </summary>
-        /// <param name="configuration">The configuration settings.</param>
-        /// <param name="clientFactory">The HTTP client factory.</param>
-        /// <param name="logger">The logger instance.</param>
-        public CalculationRunOverviewController(IConfiguration configuration,
+        /// <param name="configuration">configuration</param>
+        /// <param name="clientFactory">clientFactory</param>
+        /// <param name="logger">logger</param>
+        /// <param name="tokenAcquisition">tokenAcquisition</param>
+        /// <param name="telemetryClient">telemetryClient</param>
+        public CalculationRunOverviewController(
+            IConfiguration configuration,
             IHttpClientFactory clientFactory,
             ILogger<CalculationRunOverviewController> logger,
             ITokenAcquisition tokenAcquisition,
@@ -37,58 +36,35 @@ namespace EPR.Calculator.Frontend.Controllers
             : base(configuration, tokenAcquisition, telemetryClient)
         {
             _configuration = configuration;
-            _clientFactory = clientFactory;
-            _logger = logger;
         }
-
 
         /// <summary>
         /// Gets the calculation run overview page.
         /// </summary>
-        /// <param name="runId"></param>
-        /// <returns></returns>
+        /// <param name="runId"> Run Id.</param>
+        /// <returns>View.</returns>
         [Authorize(Roles = "SASuperUser")]
         [Route("runoverview/{runId}")]
-        public async Task<IActionResult> IndexAsync(int runId)
+        public Task<IActionResult> IndexAsync(int runId)
         {
-            try
+
+            // Get the calculation run details from the API
+            var calculatorRun = new CalculatorRunDto()
             {
-                // Get the calculation run details from the API
-                var calculatorRun = new CalculatorRunDto()
-                {
-                    RunId = 1,
-                    FinancialYear = "2024-25",
-                    FileExtension = "xlsx",
-                    RunClassificationStatus = "Draft",
-                    RunName = "Calculation run 99",
-                    RunClassificationId = 240008,
-                    CreatedAt = DateTime.Now,
-                    CreatedBy = "Jo Bloggs",
-                };
+                RunId = runId,
+                FinancialYear = "2024-25",
+                FileExtension = "xlsx",
+                RunClassificationStatus = "Draft",
+                RunName = "Calculation run 99",
+                RunClassificationId = 240008,
+                CreatedAt = DateTime.Now,
+                CreatedBy = "Jo Bloggs",
+            };
 
-                if (!IsRunEligibleForDisplay(calculatorRun))
-                {
-                    return View(ViewNames.CalculationRunDetailsErrorPage, CreateViewModelCommonData());
-                }
+            var viewModel = CreateViewModel(runId, calculatorRun);
+            SetDownloadParameters(viewModel);
 
-                var viewModel = CreateViewModel(runId, calculatorRun);
-                SetDownloadParameters(viewModel);
-
-                return View(ViewNames.CalculationRunOverviewIndex, viewModel);
-            }
-            catch (Exception ex)
-            {
-                _logger.LogError(ex, "An error occurred while processing the request.");
-                return RedirectToErrorPage();
-            }
-        }
-
-        private static bool IsRunEligibleForDisplay(CalculatorRunDto calculatorRun)
-            => calculatorRun.RunClassificationId == (int)RunClassification.UNCLASSIFIED;
-
-        private ViewModelCommonData CreateViewModelCommonData()
-        {
-            return new ViewModelCommonData { CurrentUser = CommonUtil.GetUserName(HttpContext) };
+            return Task.FromResult<IActionResult>(View(ViewNames.CalculationRunOverviewIndex, viewModel));
         }
 
         private CalculatorRunOverviewViewModel CreateViewModel(int runId, CalculatorRunDto calculatorRun)
@@ -121,8 +97,5 @@ namespace EPR.Calculator.Frontend.Controllers
             viewModel.DownloadErrorURL = $"/DownloadFileError/{viewModel.Data.RunId}";
             viewModel.DownloadTimeout = _configuration.GetValue<int>($"{ConfigSection.CalculationRunSettings}:{ConfigSection.DownloadResultTimeoutInMilliSeconds}");
         }
-
-        private IActionResult RedirectToErrorPage()
-         => RedirectToAction(ActionNames.StandardErrorIndex, CommonUtil.GetControllerName(typeof(StandardErrorController)));
     }
 }
