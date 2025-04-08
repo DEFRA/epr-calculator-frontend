c80fe9898e518f8f3fc2df811b8922021cbf345d
Rekha Devi <rekha.devi.external@eviden.com>
2025-04-08 14:03:55 +0100

    replace the view data by passing the correct model

---
+++ src/EPR.Calculator.Frontend/Constants/CalculatorRunNames.cs
@@ -0,0 +1,18 @@
+﻿namespace EPR.Calculator.Frontend.Constants
+{
+    /// <summary>
+    /// Contains constants related to the naming of calculator runs.
+    /// </summary>
+    public static class CalculatorRunNames
+    {
+        /// <summary>
+        /// The title for the when a new calculation is started.
+        /// </summary>
+        public const string Title = "You have started a calculation";
+
+        /// <summary>
+        /// The text displayed to the user indicating the time required for the calculation to complete.
+        /// </summary>
+        public const string NextText = "It will take up to 15 minutes for the calculation to finish. You will be able to view it in the list of calculations on the dashboard.";
+    }
+}
