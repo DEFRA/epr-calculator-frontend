3f08485c7c90ddd3e70364bd85ccfb4804ff2d01
David Jayakumar <david.2.jayakumar.external@eviden.com>
2025-04-10 11:13:56 +0100

    Classifying a run - Scenario 3

---
+++ src/EPR.Calculator.Frontend/ViewModels/ClassifyCalculationRunScenerio2ViewModel.cs
@@ -0,0 +1,27 @@
+﻿using EPR.Calculator.Frontend.Constants;
+using EPR.Calculator.Frontend.Models;
+using System.Diagnostics.CodeAnalysis;
+
+namespace EPR.Calculator.Frontend.ViewModels
+{
+    /// <summary>
+    /// classify calculation run scenerio1 view model.
+    /// </summary>
+    public record ClassifyCalculationRunScenerio2ViewModel : ViewModelCommonData
+    {
+        /// <summary>
+        /// Gets or sets the back link.
+        /// </summary>
+        public string BackLink { get; set; } = CommonConstants.DashBoard;
+
+        /// <summary>
+        /// Gets or sets the errors.
+        /// </summary>
+        public ErrorViewModel? Errors { get; set; }
+
+        /// <summary>
+        /// Gets the data for the run status update.
+        /// </summary>
+        public required CalculatorRunStatusUpdateDto CalculatorRunStatus { get; init; }
+    }
+}
