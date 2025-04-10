39ec7c613a27bd863b0efc690a0adbd3900d0f6e
Rekha Devi <rekha.devi.external@eviden.com>
2025-04-10 15:26:50 +0100

    Added Health Check functionality and unit test

---
+++ src/EPR.Calculator.Frontend.UnitTests/HealthCheck/HealthCheckOptionsBuilderTests.cs
@@ -0,0 +1,22 @@
+﻿using EPR.Calculator.Frontend.HealthCheck;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+namespace EPR.Calculator.Frontend.UnitTests.HealthCheck
+{
+    [TestClass]
+    public class HealthCheckOptionsBuilderTests
+    {
+        [TestMethod]
+        public void Build_ShouldReturnHealthCheckOptions_WithExpectedProperties()
+        {
+            // Act
+            var options = HealthCheckOptionsBuilder.Build();
+
+            // Assert
+            Assert.IsNotNull(options);
+            Assert.IsFalse(options.AllowCachingResponses);
+            Assert.AreEqual(StatusCodes.Status200OK, options.ResultStatusCodes[HealthStatus.Healthy]);
+        }
+    }
+}
---
+++ src/EPR.Calculator.Frontend/HealthCheck/HealthCheckOptionsBuilder.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using System.Diagnostics.CodeAnalysis;
+
+namespace EPR.Calculator.Frontend.HealthCheck
+{
+    /// <summary>
+        /// Provides a method to build <see cref="HealthCheckOptions"/> with predefined settings.
+        /// </summary>
+    [ExcludeFromCodeCoverage]
+    public class HealthCheckOptionsBuilder
+    {
+
+        /// <summary>
+                /// Builds and returns a <see cref="HealthCheckOptions"/> instance with predefined settings.
+                /// </summary>
+                /// <returns>
+                /// A <see cref="HealthCheckOptions"/> instance with <see cref="HealthCheckOptions.AllowCachingResponses"/> set to <c>false</c>
+                /// and <see cref="HealthCheckOptions.ResultStatusCodes"/> configured to return <see cref="StatusCodes.Status200OK"/> for <see cref="HealthStatus.Healthy"/>.
+                /// </returns>
+        public static HealthCheckOptions Build()
+        {
+            return new HealthCheckOptions
+            {
+                AllowCachingResponses = false,
+                ResultStatusCodes =
+                {
+                    [HealthStatus.Healthy] = StatusCodes.Status200OK,
+                },
+            };
+        }
+    }
+}
