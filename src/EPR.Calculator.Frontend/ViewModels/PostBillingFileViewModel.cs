3812138d5e7df0e20e256e2cf20618992f5add29
David Jayakumar <david.2.jayakumar.external@eviden.com>
2025-04-14 16:34:41 +0100

    Post billing file sent

---
+++ src/EPR.Calculator.Frontend/ViewModels/PostBillingFileViewModel.cs
@@ -0,0 +1,17 @@
+﻿using EPR.Calculator.Frontend.Models;
+
+namespace EPR.Calculator.Frontend.ViewModels
+{
+    public record PostBillingFileViewModel : ViewModelCommonData
+    {
+        public required string BillingFileSentDate { get; set; }
+
+        public required string BillingFileRunBy { get; set; }
+
+        public required string BillingFileSentBy { get; set; }
+
+        public string? SelectedCalcRunOption { get; set; }
+
+        public required CalculatorRunStatusUpdateDto CalculatorRunStatus { get; init; }
+    }
+}
