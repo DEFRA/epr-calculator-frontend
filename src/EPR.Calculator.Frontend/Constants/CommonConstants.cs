90e563680e2e35d5fab0b23a0d69c2786aa5865f
David Jayakumar <david.2.jayakumar.external@eviden.com>
2025-04-24 10:56:17 +0100

    538760 Change invoice to billing (#199)

--- src/EPR.Calculator.Frontend/Constants/CommonConstants.cs
+++ src/EPR.Calculator.Frontend/Constants/CommonConstants.cs
@@ -5,7 +5,7 @@
         public const string ShowDetailedError = "ShowDetailedError";
 
         public const string SendBillingFile = "Send the billing file";
-        public const string ConfirmationContent = "You have accepted all invoice instructions. Send the billing file to the FSS.";
+        public const string ConfirmationContent = "You have accepted all billing instructions. Send the billing file to the FSS.";
         public const string WarningContent = "Once sent, you will not be able to amend the billing file.";
 
         public const string TimeZone = "GMT Standard Time";
