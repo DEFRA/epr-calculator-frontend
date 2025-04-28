e58a3f4a31964510ee2939e9b19ace9db8b62276
Santhosh kumar <111443660+Skumarhpa@users.noreply.github.com>
2025-04-28 13:56:23 +0100

    Add enum

--- src/EPR.Calculator.Frontend/Enums/RunClassification.cs
+++ src/EPR.Calculator.Frontend/Enums/RunClassification.cs
@@ -15,13 +15,28 @@ namespace EPR.Calculator.Frontend.Enums
         [EnumMember(Value = "UNCLASSIFIED")]
         UNCLASSIFIED = 3,
 
-        [EnumMember(Value = "PLAY")]
-        PLAY = 4,
+        [EnumMember(Value = "TEST RUN")]
+        TESTRUN = 4,
 
         [EnumMember(Value = "ERROR")]
         ERROR = 5,
 
         [EnumMember(Value = "DELETED")]
         DELETED = 6,
+
+        [EnumMember(Value = "INITIAL RUN COMPLETED")]
+        INITIALRUNCOMPLETED = 7,
+
+        [EnumMember(Value = "INITIAL RUN")]
+        INITIALRUN = 8,
+
+        [EnumMember(Value = "INTERIM RE-CALCULATION RUN")]
+        INTERIMRECALCULATIONRUN = 9,
+
+        [EnumMember(Value = "FINAL RUN")]
+        FINALRUN = 10,
+
+        [EnumMember(Value = "FINAL RE-CALCULATION RUN")]
+        FINALRECALCULATIONRUN = 11,
     }
 }
