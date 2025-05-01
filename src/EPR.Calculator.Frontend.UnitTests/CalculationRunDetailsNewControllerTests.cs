11ec4252c7dca452a5d9d1d20e8006a012c8e6bd
Santhosh kumar <111443660+Skumarhpa@users.noreply.github.com>
2025-05-01 15:11:04 +0100

    add unit tests

--- src/EPR.Calculator.Frontend.UnitTests/CalculationRunDetailsNewControllerTests.cs
+++ src/EPR.Calculator.Frontend.UnitTests/CalculationRunDetailsNewControllerTests.cs
@@ -338,6 +338,50 @@ namespace EPR.Calculator.Frontend.UnitTests
             Assert.AreEqual(1, result.RouteValues["runId"]);
         }
 
+        [TestMethod]
+        public async Task Submit_ValidModelState_NoOutput_ReturnsRedirectToAction()
+        {
+            // Arrange
+            var model = new CalculatorRunDetailsNewViewModel()
+            {
+                CalculatorRunDetails = new CalculatorRunDetailsViewModel()
+                {
+                    RunId = 1,
+                    RunName = "Test Run"
+                },
+                SelectedCalcRunOption = null
+            };
+
+            var mockHttpMessageHandler = new Mock<HttpMessageHandler>();
+            mockHttpMessageHandler
+                .Protected()
+                .Setup<Task<HttpResponseMessage>>(
+                    "SendAsync",
+                    ItExpr.IsAny<HttpRequestMessage>(),
+                    ItExpr.IsAny<CancellationToken>()).ReturnsAsync(new HttpResponseMessage
+                    {
+                        StatusCode = HttpStatusCode.OK,
+                        Content = new StringContent(JsonConvert.SerializeObject(MockData.GetCalculatorRun())),
+                    });
+
+            var httpClient = new HttpClient(mockHttpMessageHandler.Object);
+            var mockHttpClientFactory = new Mock<IHttpClientFactory>();
+            mockHttpClientFactory
+                .Setup(_ => _.CreateClient(It.IsAny<string>()))
+                    .Returns(httpClient);
+            var config = GetConfigurationValues();
+            var mockTokenAcquisition = new Mock<ITokenAcquisition>();
+            var controller = new CalculationRunDetailsNewController(config, mockHttpClientFactory.Object, _mockLogger.Object,
+                mockTokenAcquisition.Object, new TelemetryClient());
+            // Act
+            var result = await _controller.Submit(model) as RedirectToActionResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual(ActionNames.Index, result.ActionName);
+            Assert.AreEqual(1, result.RouteValues["runId"]);
+        }
+
         private static IConfiguration GetConfigurationValues()
         {
             string projectPath = AppDomain.CurrentDomain.BaseDirectory.Split(Separator, StringSplitOptions.None)[0];
