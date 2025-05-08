dc517ae3f9e039b030b3bc4097fd0dea97f805fa
Shilpi Goel <shilpi.goel.external@eviden.com>
2025-05-08 15:14:23 +0100

    Changes for date format

---
+++ src/EPR.Calculator.Frontend/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,17 @@
+﻿namespace EPR.Calculator.Frontend.Extensions
+{
+    public static class DateTimeExtensions
+    {
+        /// <summary>
+        /// Converts a UTC DateTime to a UK DateTime string format.
+        /// </summary>
+        /// <param name="dateTime"></param>
+        /// <returns></returns>
+        public static string ToUKDateTimeDisplay(this DateTime dateTime)
+        {
+            var ukTimeZone = TimeZoneInfo.FindSystemTimeZoneById("GMT Standard Time");
+            var ukTime = TimeZoneInfo.ConvertTimeFromUtc(DateTime.SpecifyKind(dateTime, DateTimeKind.Utc), ukTimeZone);
+            return ukTime.ToString("dd MMM yyyy 'at' HH:mm");
+        }
+    }
+}
