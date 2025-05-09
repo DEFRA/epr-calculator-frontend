0ad08aaec3719a6464b530280df66057d7a613f7
Shilpi Goel <shilpi.goel.external@eviden.com>
2025-05-09 10:38:32 +0100

    Covered Unit Tests

--- src/EPR.Calculator.Frontend/Extensions/DateTimeExtensions.cs
+++ src/EPR.Calculator.Frontend/Extensions/DateTimeExtensions.cs
@@ -9,9 +9,9 @@
         /// <returns></returns>
         public static string ToUKDateTimeDisplay(this DateTime dateTime)
         {
-            var ukTimeZone = TimeZoneInfo.FindSystemTimeZoneById("GMT Standard Time");
-            var ukTime = TimeZoneInfo.ConvertTimeFromUtc(DateTime.SpecifyKind(dateTime, DateTimeKind.Utc), ukTimeZone);
-            return ukTime.ToString("dd MMM yyyy 'at' HH:mm");
+            var britishZone = TimeZoneInfo.FindSystemTimeZoneById("GMT Standard Time");
+            var britishTime = TimeZoneInfo.ConvertTimeFromUtc(DateTime.SpecifyKind(dateTime, DateTimeKind.Utc), britishZone);
+            return britishTime.ToString("dd MMM yyyy 'at' HH:mm");
         }
     }
 }
