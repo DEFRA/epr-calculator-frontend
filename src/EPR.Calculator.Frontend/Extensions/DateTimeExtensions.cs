1fc465239a578a144369c8bd6b70585f994f7a2e
Shilpi Goel <shilpi.goel.external@eviden.com>
2025-05-09 14:12:17 +0100

    Changes for date format (#219)
    * Changes for date format
    
    * fix the issue
    
    * Covered Unit Tests
    
    * Few more changes for the Datetime extension

---
+++ src/EPR.Calculator.Frontend/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,24 @@
+﻿using EPR.Calculator.Frontend.Constants;
+
+namespace EPR.Calculator.Frontend.Extensions
+{
+    public static class DateTimeExtensions
+    {
+        /// <summary>
+        /// Converts a UTC DateTime to a UK DateTime string format.
+        /// </summary>
+        /// <param name="dateTime"></param>
+        /// <returns></returns>
+        public static string ToUKDateTimeDisplay(this DateTime dateTime)
+        {
+            // Get UK time zone, which automatically adjusts for BST
+            var britishZone = TimeZoneInfo.FindSystemTimeZoneById(CommonConstants.TimeZone);
+
+            // Ensure the input is treated as UTC and  // Convert to UK local time (BST or GMT depending on the date)
+            var britishTime = TimeZoneInfo.ConvertTimeFromUtc(DateTime.SpecifyKind(dateTime, DateTimeKind.Utc), britishZone);
+
+            // Format the date and time
+            return britishTime.ToString($"{CommonConstants.DateFormat} 'at' {CommonConstants.TimeFormat}");
+        }
+    }
+}
