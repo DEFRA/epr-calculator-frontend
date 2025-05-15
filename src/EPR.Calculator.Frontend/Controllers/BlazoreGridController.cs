43efa9cad048aef075af9c7c619793f7a6600ac7
Santhosh kumar <111443660+Skumarhpa@users.noreply.github.com>
2025-05-15 09:41:22 +0100

    Fix Sonar issue

--- src/EPR.Calculator.Frontend/Controllers/BlazoreGridController.cs
+++ src/EPR.Calculator.Frontend/Controllers/BlazoreGridController.cs
@@ -22,9 +22,13 @@ namespace EPR.Calculator.Frontend.Controllers
     public record OrgProducerData
     {
         public string OrganisationName { get; set; }
+
         public string OrganisationID { get; set; }
+
         public string BillingInstructions { get; set; }
+
         public string InvoiceAmount { get; set; }
+
         public string Status { get; set; }
     }
 }
