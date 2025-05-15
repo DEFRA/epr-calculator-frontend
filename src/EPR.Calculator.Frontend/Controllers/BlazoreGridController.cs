5a8de7597363a00184bf47da304855f32b4fb72c
Santhosh kumar <111443660+Skumarhpa@users.noreply.github.com>
2025-05-15 09:33:03 +0100

    Add pagination

--- src/EPR.Calculator.Frontend/Controllers/BlazoreGridController.cs
+++ src/EPR.Calculator.Frontend/Controllers/BlazoreGridController.cs
@@ -10,5 +10,21 @@ namespace EPR.Calculator.Frontend.Controllers
         {
             return View();
         }
+
+        [HttpPost]
+        [Route("blazoreGrid/submit")]
+        public IActionResult Submit([FromBody] List<OrgProducerData> model)
+        {
+            return Json(new { message = "Success" });
+        }
+    }
+
+    public record OrgProducerData
+    {
+        public string OrganisationName { get; set; }
+        public string OrganisationID { get; set; }
+        public string BillingInstructions { get; set; }
+        public string InvoiceAmount { get; set; }
+        public string Status { get; set; }
     }
 }
