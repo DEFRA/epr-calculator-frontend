4798948d81d26256dbbbb1b295c149d1cd311c54
Tim Powell <timothy.powell@eviden.com>
2025-05-22 09:28:46 +0100

    "Delete run" button now calls the API and sets the run's status to deleted

--- src/EPR.Calculator.Frontend.UnitTests/CalculationRunDeleteControllerTests.cs
+++ src/EPR.Calculator.Frontend.UnitTests/CalculationRunDeleteControllerTests.cs
@@ -1,4 +1,5 @@
-﻿using AutoFixture;
+﻿using System.Net;
+using AutoFixture;
 using EPR.Calculator.Frontend.Constants;
 using EPR.Calculator.Frontend.Controllers;
 using EPR.Calculator.Frontend.UnitTests.HelpersTest;
@@ -7,7 +8,6 @@ using Microsoft.ApplicationInsights;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
 using Microsoft.Identity.Web;
 using Moq;
 
@@ -16,29 +16,25 @@ namespace EPR.Calculator.Frontend.UnitTests
     [TestClass]
     public class CalculationRunDeleteControllerTests
     {
-        private readonly IConfiguration _configuration = ConfigurationItems.GetConfigurationValues();
-        private Mock<IHttpClientFactory> _mockClientFactory;
-        private Mock<ILogger<CalculationRunDeleteController>> _mockLogger;
-        private TelemetryClient _mockTelemetryClient;
-
         public CalculationRunDeleteControllerTests()
         {
             this.Fixture = new Fixture();
+            this.Configuration = ConfigurationItems.GetConfigurationValues();
             this.MockHttpContext = new Mock<HttpContext>();
             this.MockHttpContext.Setup(c => c.User.Identity.Name).Returns(Fixture.Create<string>);
+            this.MockMessageHandler = TestMockUtils.BuildMockMessageHandler(HttpStatusCode.Created);
+            MockClientFactory = TestMockUtils.BuildMockHttpClientFactory(MockMessageHandler.Object);
         }
 
         private Fixture Fixture { get; init; }
 
+        private IConfiguration Configuration { get; init; }
+
         private Mock<HttpContext> MockHttpContext { get; init; }
 
-        [TestInitialize]
-        public void Setup()
-        {
-            _mockClientFactory = new Mock<IHttpClientFactory>();
-            _mockLogger = new Mock<ILogger<CalculationRunDeleteController>>();
-            _mockTelemetryClient = new TelemetryClient();
-        }
+        private Mock<HttpMessageHandler> MockMessageHandler { get; init; }
+
+        private Mock<IHttpClientFactory> MockClientFactory { get; init; }
 
         [TestMethod]
         public void CalculationRunDelete_Success_View_Test()
@@ -46,8 +42,11 @@ namespace EPR.Calculator.Frontend.UnitTests
             // Arrange
             int runId = this.Fixture.Create<int>();
 
-            var controller = new CalculationRunDeleteController(_configuration, _mockClientFactory.Object,
-                new Mock<ITokenAcquisition>().Object, _mockTelemetryClient);
+            var controller = new CalculationRunDeleteController(
+                this.Configuration,
+                this.MockClientFactory.Object,
+                new Mock<ITokenAcquisition>().Object,
+                new TelemetryClient());
             controller.ControllerContext.HttpContext = this.MockHttpContext.Object;
 
             // Act
@@ -60,22 +59,94 @@ namespace EPR.Calculator.Frontend.UnitTests
             Assert.AreEqual(runId, resultModel.CalculatorRunStatusData.RunId);
         }
 
+        /// <summary>
+        /// Test that the DeleteConfirmationSuccess method redirects to the confirmation screen
+        /// when the API returns success.
+        /// </summary>
+        /// <returns>A <see cref="Task"/>.</returns>
         [TestMethod]
-        public void CalculationRunDelete_Confirmation_Success_View_Test()
+        public async Task DeleteConfirmationSuccess_RedirectToConfirmationScreenWhenDeletionSucceeds()
         {
             // Arrange
-            var controller = new CalculationRunDeleteController(_configuration, _mockClientFactory.Object,
-                new Mock<ITokenAcquisition>().Object, _mockTelemetryClient);
+            var controller = new CalculationRunDeleteController(
+                this.Configuration,
+                this.MockClientFactory.Object,
+                new Mock<ITokenAcquisition>().Object,
+                new TelemetryClient());
             controller.ControllerContext.HttpContext = this.MockHttpContext.Object;
+            var model = new CalculatorRunDetailsViewModel
+            {
+                RunId = this.Fixture.Create<int>(),
+                RunName = this.Fixture.Create<string>(),
+            };
 
             // Act
-            var result = controller.DeleteConfirmationSuccess() as ViewResult;
+            var result = await controller.DeleteConfirmationSuccess(model) as ViewResult;
 
             // Assert
-            var resultModel = result.Model;
             Assert.IsNotNull(result);
             Assert.AreEqual(ViewNames.CalculationRunDeleteConfirmationSuccess, result.ViewName);
-            Assert.IsTrue(typeof(string) == resultModel.GetType());
+            Assert.IsTrue(typeof(string) == result.Model.GetType());
+        }
+
+        /// <summary>
+        /// Tests that the DeleteConfirmationSuccess method
+        /// redirects to the error page when the API returns a non-success status code.
+        /// </summary>
+        /// <returns>A <see cref="Task"/>.</returns>
+        [TestMethod]
+        public async Task DeleteConfirmationSuccess_RedirectToErrorWhenApiDoesntReturnSuccess()
+        {
+            // Arrange
+            var controller = new CalculationRunDeleteController(
+                this.Configuration,
+                TestMockUtils.BuildMockHttpClientFactory(HttpStatusCode.BadRequest).Object,
+                new Mock<ITokenAcquisition>().Object,
+                new TelemetryClient());
+            controller.ControllerContext.HttpContext = this.MockHttpContext.Object;
+            var model = new CalculatorRunDetailsViewModel
+            {
+                RunId = this.Fixture.Create<int>(),
+                RunName = this.Fixture.Create<string>(),
+            };
+
+            // Act
+            var result = await controller.DeleteConfirmationSuccess(model) as RedirectToActionResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual(ActionNames.StandardErrorIndex, result.ActionName);
+        }
+
+        /// <summary>
+        /// Tests that the DeleteConfirmationSuccess method redirects to the error 
+        /// page when the API throws an exception.
+        /// </summary>
+        /// <returns>A <see cref="Task"/>.</returns>
+        [TestMethod]
+        public async Task DeleteConfirmationSuccess_RedirectToErrorWhenApiThrowsException()
+        {
+            // Arrange
+            var messageHandler = TestMockUtils.BuildMockMessageHandler(shouldThrowException: true).Object;
+
+            var controller = new CalculationRunDeleteController(
+                this.Configuration,
+                TestMockUtils.BuildMockHttpClientFactory(messageHandler).Object,
+                new Mock<ITokenAcquisition>().Object,
+                new TelemetryClient());
+            controller.ControllerContext.HttpContext = this.MockHttpContext.Object;
+            var model = new CalculatorRunDetailsViewModel
+            {
+                RunId = this.Fixture.Create<int>(),
+                RunName = this.Fixture.Create<string>(),
+            };
+
+            // Act
+            var result = await controller.DeleteConfirmationSuccess(model) as RedirectToActionResult;
+
+            // Assert
+            Assert.IsInstanceOfType<RedirectToActionResult>(result);
+            Assert.AreEqual(ActionNames.StandardErrorIndex, result.ActionName);
         }
     }
 }
