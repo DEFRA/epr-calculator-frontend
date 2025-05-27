7e5e28ed1e80d8c4060051705d3df01a2fc1a870
Mazar Shaik <mazar.shaik.external@eviden.com>
2025-05-27 09:52:21 +0100

    Initial checkin for Accept/Reject Confirmation screen

---
+++ src/EPR.Calculator.Frontend.UnitTests/Controllers/AcceptRejectConfirmationControllerTests.cs
@@ -0,0 +1,61 @@
+﻿using EPR.Calculator.Frontend.Controllers;
+using EPR.Calculator.Frontend.UnitTests.HelpersTest;
+using EPR.Calculator.Frontend.ViewModels;
+using Microsoft.ApplicationInsights;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Identity.Web;
+using Moq;
+
+namespace EPR.Calculator.Frontend.UnitTests.Controllers
+{
+    [TestClass]
+    public class AcceptRejectConfirmationControllerTests
+    {
+        private readonly IConfiguration _configuration = ConfigurationItems.GetConfigurationValues();
+        private Mock<ITokenAcquisition> _mockTokenAcquisition;
+        private TelemetryClient _telemetryClient;
+        private AcceptRejectConfirmationController _controller;
+        private Mock<HttpContext> _mockHttpContext;
+
+        public AcceptRejectConfirmationControllerTests()
+        {
+            _mockTokenAcquisition = new Mock<ITokenAcquisition>();
+            _telemetryClient = new TelemetryClient();
+            _mockHttpContext = new Mock<HttpContext>();
+
+            _controller = new AcceptRejectConfirmationController(
+                   _configuration,
+                   _mockTokenAcquisition.Object,
+                   _telemetryClient,
+                   new Mock<IHttpClientFactory>().Object)
+            {
+                // Setting the mocked HttpContext for the controller
+                ControllerContext = new ControllerContext
+                {
+                    HttpContext = _mockHttpContext.Object
+                }
+            };
+        }
+
+        [TestMethod]
+        public void CanCallIndex()
+        {
+            int runId = 99;
+
+            // Mocking HttpContext.User.Identity.Name to simulate a logged-in user
+            _mockHttpContext.Setup(ctx => ctx.User.Identity.Name).Returns("TestUser");
+
+            var result = _controller.Index(runId);
+
+            // Assert
+            Assert.IsNotNull(result);
+
+            Assert.IsInstanceOfType(result, typeof(ViewResult));
+            var viewResult = result as ViewResult;
+            Assert.IsNotNull(viewResult);
+            Assert.IsInstanceOfType(viewResult.Model, typeof(AcceptRejectConfirmationViewModel));
+        }
+    }
+}
---
+++ src/EPR.Calculator.Frontend/ViewModels/AcceptRejectConfirmationViewModel.cs
@@ -0,0 +1,17 @@
+﻿namespace EPR.Calculator.Frontend.ViewModels
+{
+    /// <summary>
+    /// The view model for accept reject confirmation view model.
+    /// </summary>
+    public record AcceptRejectConfirmationViewModel : ViewModelCommonData
+    {
+        /// <summary>
+        /// Gets or sets the run id.
+        public int RunId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the calculation run title.
+        /// </summary>
+        public string CalculationRunTitle { get; set; } = string.Empty;
+    }
+}
