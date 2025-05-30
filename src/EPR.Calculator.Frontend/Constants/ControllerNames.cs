783180d768c07da903b942854d6d60e070e35495
John Jebastin Benjamin <john.2.benjamin.external@eviden.com>
2025-05-30 14:26:06 +0100

    561275 Rerun calculation link fix (#239)
    Co-authored-by: Uday Denduluri <uday.2.denduluri.external@eviden.com>

--- src/EPR.Calculator.Frontend/Constants/ControllerNames.cs
+++ src/EPR.Calculator.Frontend/Constants/ControllerNames.cs
@@ -1,27 +1,68 @@
 ﻿namespace EPR.Calculator.Frontend.Constants
 {
+    /// <summary>
+    /// Controller names.
+    /// </summary>
     public static class ControllerNames
     {
-        public const string Dashboard = "Dashboard";
-
-        public const string CalculationRunDetails = "CalculationRunDetailsNew";
-
-        public const string ClassifyingCalculationRun = "SetRunClassification";
-
-        public const string ClassifyRunConfirmation = "ClassifyRunConfirmation";
-
-        public const string PaymentCalculator = "PaymentCalculator";
-
-        public const string CalculationRunOverview = "CalculationRunOverview";
-
-        public const string SendBillingFile = "SendBillingFile";
-
-        public const string CalculationRunDelete = "CalculationRunDelete";
-
-        public const string StandardErrorController = "StandardErrorController";
-
+        /// <summary>
+        /// Account controller.
+        /// </summary>
         public const string Account = "Account";
 
+        /// <summary>
+        /// Calculation run delete controller.
+        /// </summary>
+        public const string CalculationRunDelete = "CalculationRunDelete";
+
+        /// <summary>
+        /// Calculator run details new controller.
+        /// </summary>
+        public const string CalculationRunDetails = "CalculationRunDetailsNew";
+
+        /// <summary>
+        /// Calculation run overview controller.
+        /// </summary>
+        public const string CalculationRunOverview = "CalculationRunOverview";
+
+        /// <summary>
+        /// Classifying calculation run controller.
+        /// </summary>
+        public const string ClassifyingCalculationRun = "SetRunClassification";
+
+        /// <summary>
+        /// Classify run confirmation controller.
+        /// </summary>
+        public const string ClassifyRunConfirmation = "ClassifyRunConfirmation";
+
+        /// <summary>
+        /// Dashboard controller.
+        /// </summary>
+        public const string Dashboard = "Dashboard";
+
+        /// <summary>
+        /// Organisation controller.
+        /// </summary>
         public const string Organisation = "Organisation";
+
+        /// <summary>
+        /// Payment calculator controller.
+        /// </summary>
+        public const string PaymentCalculator = "PaymentCalculator";
+
+        /// <summary>
+        /// Run a new calculation controller.
+        /// </summary>
+        public const string RunANewCalculation = "RunANewCalculation";
+
+        /// <summary>
+        /// Send billing file controller.
+        /// </summary>
+        public const string SendBillingFile = "SendBillingFile";
+
+        /// <summary>
+        /// Standard error controller.
+        /// </summary>
+        public const string StandardErrorController = "StandardErrorController";
     }
 }
