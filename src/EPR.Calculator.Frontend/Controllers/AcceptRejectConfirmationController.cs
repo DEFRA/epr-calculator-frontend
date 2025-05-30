a73e46453d01e54cede54ddc5196233e9a32a7c3
Mazar-Shaik <mazar.shaik.external@eviden.com>
2025-05-30 14:41:16 +0100

    Feature/551609 accept reject confirm screen (#236)
    * added Table with Reusable Pagination
    
    * corrected model
    
    * Unit tests
    
    * corrected partial
    
    * Add total Records
    
    * correct Unit tests
    
    * Few changes
    
    * corrected file name
    
    * Renamed model
    
    * Corrected as per SonarQube
    
    * Initial checkin for Accept/Reject Confirmation screen
    
    * Changed from set to init for AcceptRejectConfirmationViewModel record
    
    * fixed accessibility and other issues.
    
    * Underline the selected page
    
    ---------
    
    Co-authored-by: Shilpi Goel <Shilpi.Goel@goaco.com>
    Co-authored-by: Shilpi Goel <shilpi.goel.external@eviden.com>

---
+++ src/EPR.Calculator.Frontend/Controllers/AcceptRejectConfirmationController.cs
@@ -0,0 +1,53 @@
+﻿using EPR.Calculator.Frontend.Constants;
+using EPR.Calculator.Frontend.Helpers;
+using EPR.Calculator.Frontend.ViewModels;
+using Microsoft.ApplicationInsights;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Identity.Web;
+
+namespace EPR.Calculator.Frontend.Controllers
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="AcceptRejectConfirmationController"/> class.
+    /// </summary>
+    [Route("[controller]")]
+    public class AcceptRejectConfirmationController(
+        IConfiguration configuration,
+        ITokenAcquisition tokenAcquisition,
+        TelemetryClient telemetryClient,
+        IHttpClientFactory httpClientFactory)
+        : BaseController(configuration, tokenAcquisition, telemetryClient, httpClientFactory)
+    {
+        /// <summary>
+        /// Displays the accept reject confirmation controller index view.
+        /// </summary>
+        /// <param name="runId">The ID of the calculation run.</param>
+        /// <returns>accept reject confirmation index view.</returns>
+        [Route("{runId}")]
+        public IActionResult Index(int runId)
+        {
+            var viewModel = new AcceptRejectConfirmationViewModel()
+            {
+                CurrentUser = CommonUtil.GetUserName(this.HttpContext),
+                RunId = runId,
+                BackLink = ControllerNames.Organisation,
+                CalculationRunTitle = "Calculation run 99",
+            };
+
+            return this.View(ViewNames.AcceptRejectConfirmationIndex, viewModel);
+        }
+
+        /// <summary>
+        /// Handles the submission of accept all selected yes or no billing instructions.
+        /// </summary>
+        /// <returns>
+        /// A redirection to the organisation view on selection of yes or no.
+        /// </returns>
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Submit()
+        {
+            return this.RedirectToAction(ActionNames.Index, ControllerNames.Organisation);
+        }
+    }
+}
