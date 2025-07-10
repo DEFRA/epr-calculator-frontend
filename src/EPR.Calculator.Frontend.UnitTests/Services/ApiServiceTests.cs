24e36fcbe60df7d03a241efd7e6ea5bd6c5732d1
Tim Powell <timothy.powell@eviden.com>
2025-07-10 16:48:30 +0100

    Add more unit tests

--- src/EPR.Calculator.Frontend.UnitTests/Services/ApiServiceTests.cs
+++ src/EPR.Calculator.Frontend.UnitTests/Services/ApiServiceTests.cs
@@ -1,16 +1,18 @@
 ﻿namespace EPR.Calculator.Frontend.UnitTests.Services
 {
-    using System;
-    using System.Net.Http;
-    using System.Threading.Tasks;
     using AutoFixture;
     using EPR.Calculator.Frontend.Services;
     using Microsoft.ApplicationInsights;
     using Microsoft.AspNetCore.Http;
+    using Microsoft.DotNet.Scaffolding.Shared.Messaging;
     using Microsoft.Extensions.Configuration;
     using Microsoft.Identity.Web;
     using Microsoft.VisualStudio.TestTools.UnitTesting;
     using Moq;
+    using System;
+    using System.Net.Http;
+    using System.Security.Claims;
+    using System.Threading.Tasks;
 
     [TestClass]
     public class ApiServiceTests
@@ -20,7 +22,18 @@
             this.Fixture = new Fixture();
 
             this.MockHttpContext = new Mock<HttpContext>();
-            this.MockHttpContext.Setup(c => c.Session).Returns(TestMockUtils.BuildMockSession(this.Fixture).Object);
+            this.MockSession = TestMockUtils.BuildMockSession(this.Fixture);
+            this.MockHttpContext.Setup(c => c.Session).Returns(this.MockSession.Object);
+            this.Token = this.Fixture.Create<string>();
+            this.MockTokenAcquisition = new Mock<ITokenAcquisition>();
+            this.MockTokenAcquisition.Setup(t => t.GetAccessTokenForUserAsync(
+                It.IsAny<IEnumerable<string>>(),
+                It.IsAny<string?>(),
+                It.IsAny<string?>(),
+                It.IsAny<ClaimsPrincipal>(),
+                It.IsAny<TokenAcquisitionOptions>()))
+                .ReturnsAsync(this.Token);
+
             this.Configuration = TestMockUtils.BuildConfiguration();
             var messageHandler = TestMockUtils.BuildMockMessageHandler();
 
@@ -28,7 +41,7 @@
                 this.Configuration,
                 Fixture.Create<TelemetryClient>(),
                 TestMockUtils.BuildMockHttpClientFactory(messageHandler.Object).Object,
-                new Mock<ITokenAcquisition>().Object);
+                this.MockTokenAcquisition.Object);
         }
 
         private Fixture Fixture { get; init; }
@@ -38,6 +51,12 @@
         private Mock<HttpContext> MockHttpContext { get; init; }
 
         private IConfiguration Configuration { get; init; }
+
+        private Mock<ISession> MockSession { get; init; }
+
+        private Mock<ITokenAcquisition> MockTokenAcquisition { get; init; }
+
+        private string Token { get; init; }
 
         [TestMethod]
         public async Task CanCallCallApi()
@@ -55,6 +74,69 @@
                 apiUrl,
                 argument,
                 body);
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public async Task CallApi_NoTokenInSession()
+        {
+            // Arrange
+            var httpMethod = HttpMethod.Get;
+            var apiUrl = Fixture.Create<Uri>();
+            var argument = Fixture.Create<string>();
+            var body = Fixture.Create<object>();
+            byte[]? a = null;
+            this.MockSession.Setup(s => s.TryGetValue("accessToken", out a))
+                .Returns(false);
+
+            // Act
+            var result = await this.TestClass.CallApi(
+                this.MockHttpContext.Object,
+                httpMethod,
+                apiUrl,
+                argument,
+                body).Result.Content.ReadAsStringAsync();
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public async Task CallApi_CantRetrieveToken()
+        {
+            // Arrange
+            var httpMethod = HttpMethod.Get;
+            var apiUrl = Fixture.Create<Uri>();
+            var argument = Fixture.Create<string>();
+            var body = Fixture.Create<object>();
+            byte[]? a = null;
+            this.MockSession.Setup(s => s.TryGetValue("accessToken", out a))
+                .Returns(false);
+            this.Configuration.GetSection("DownstreamApi:Scopes").Value = null;
+
+            var testClass = new ApiService(
+                null,
+                Fixture.Create<TelemetryClient>(),
+                TestMockUtils.BuildMockHttpClientFactory(TestMockUtils.BuildMockMessageHandler().Object).Object,
+                this.MockTokenAcquisition.Object);
+
+            // Act
+            Exception result = null;
+            try
+            {
+                await testClass.CallApi(
+                    this.MockHttpContext.Object,
+                    httpMethod,
+                    apiUrl,
+                    argument,
+                    body).Result.Content.ReadAsStringAsync();
+            }
+            catch (Exception ex)
+            {
+                result = ex;
+            }
 
             // Assert
             Assert.IsNotNull(result);
