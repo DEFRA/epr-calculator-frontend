a943f053b70c3f878e09ade1225c7fad86ec6095
Tim Powell <timothy.powell@eviden.com>
2025-07-10 15:43:50 +0100

    Add more unit tests

---
+++ src/EPR.Calculator.Frontend.UnitTests/Services/ApiServiceTests.cs
@@ -0,0 +1,82 @@
+﻿namespace EPR.Calculator.Frontend.UnitTests.Services
+{
+    using AutoFixture;
+    using AutoFixture.AutoMoq;
+    using EPR.Calculator.Frontend.Services;
+    using EPR.Calculator.Frontend.UnitTests.HelpersTest;
+    using Microsoft.ApplicationInsights;
+    using Microsoft.ApplicationInsights.Extensibility.Implementation;
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Identity.Web;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Moq;
+    using System;
+    using System.Net.Http;
+    using System.Threading.Tasks;
+
+    [TestClass]
+    public class ApiServiceTests
+    {
+        public ApiServiceTests()
+        {
+            this.Fixture = new Fixture();
+
+            this.MockHttpContext = new Mock<HttpContext>();
+            this.MockHttpContext.Setup(c => c.Session).Returns(TestMockUtils.BuildMockSession(this.Fixture).Object);
+            this.Configuration = TestMockUtils.BuildConfiguration();
+            var messageHandler = TestMockUtils.BuildMockMessageHandler();
+
+            this.TestClass = new ApiService(
+                this.Configuration,
+                Fixture.Create<TelemetryClient>(),
+                TestMockUtils.BuildMockHttpClientFactory(messageHandler.Object).Object,
+                new Mock<ITokenAcquisition>().Object);
+        }
+
+        private Fixture Fixture { get; init; }
+
+        private ApiService TestClass { get; init; }
+
+        private Mock<HttpContext> MockHttpContext { get; init; }
+
+        private IConfiguration Configuration { get; init; }
+
+        [TestMethod]
+        public async Task CanCallCallApi()
+        {
+            // Arrange
+            var httpMethod = HttpMethod.Get;
+            var apiUrl = Fixture.Create<Uri>();
+            var argument = Fixture.Create<string>();
+            var body = Fixture.Create<object>();
+
+            // Act
+            var result = await this.TestClass.CallApi(
+                this.MockHttpContext.Object,
+                httpMethod,
+                apiUrl,
+                argument,
+                body);
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public void CanCallGetApiUrl()
+        {
+            // Arrange
+            var configSection = this.Configuration;
+            var configKey = Fixture.Create<string>();
+
+            // Act
+            var result = this.TestClass.GetApiUrl("ParameterSettings", "DefaultParameterSettingsApi");
+
+            // Assert
+            Assert.AreEqual(
+                Configuration.GetSection("ParameterSettings").GetValue<string>("DefaultParameterSettingsApi"),
+                result.ToString());
+        }
+    }
+}
