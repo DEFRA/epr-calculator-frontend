1b09e0193c4d083c3d32f0cc3e91555a4ecc270e
David Jayakumar <david.2.jayakumar.external@eviden.com>
2025-07-12 11:03:42 +0100

    553295 Accept/Reject Progress banner

--- src/EPR.Calculator.Frontend/Constants/BillingInstructionConstants.cs
+++ src/EPR.Calculator.Frontend/Constants/BillingInstructionConstants.cs
@@ -13,5 +13,7 @@
         public const string OrganisationIdKey = "organisationId";
 
         public const string BillingStatus = "billingStatus";
+
+        public const string BillingStatusBanner = "You’ve accepted {0} instructions and rejected {1}. <br>{0} instructions are still pending.";
     }
 }
