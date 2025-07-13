cadb19d3ff3e3509e0ae1f11304d2d4f42887f88
David Jayakumar <david.2.jayakumar.external@eviden.com>
2025-07-13 10:55:37 +0100

    Add next step

--- src/EPR.Calculator.Frontend/Constants/BillingInstructionConstants.cs
+++ src/EPR.Calculator.Frontend/Constants/BillingInstructionConstants.cs
@@ -15,5 +15,9 @@
         public const string BillingStatus = "billingStatus";
 
         public const string BillingStatusBanner = "You’ve accepted {0} instructions and rejected {1}. <br>{2} instructions are still pending.";
+
+        public const string BillingNextStep = "Next step";
+
+        public const string BillingNextStepText = "If you’re happy with all billing instructions, you’ll be able to generate a draft billing file. You can return to this screen to make changes before the billing file is sent to the FSS.";
     }
 }
