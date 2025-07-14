73b10d538d41b130c11baa7b087b253eccb7d172
Santhosh kumar <111443660+Skumarhpa@users.noreply.github.com>
2025-07-14 15:18:02 +0100

    Implement the reject screen

---
+++ src/EPR.Calculator.Frontend/Controllers/ReasonForRejectionController.cs
@@ -0,0 +1,52 @@
+﻿using EPR.Calculator.Frontend.Constants;
+using EPR.Calculator.Frontend.ViewModels;
+using Microsoft.ApplicationInsights;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Identity.Web;
+using System.Configuration;
+using System.Net.Http;
+
+namespace EPR.Calculator.Frontend.Controllers
+{
+    /// <summary>
+    /// Controller for reason for rejection
+    /// </summary>
+    [Route("[controller]")]
+    public class ReasonForRejectionController(
+        IConfiguration configuration,
+        ITokenAcquisition tokenAcquisition,
+        TelemetryClient telemetryClient,
+        IHttpClientFactory httpClientFactory)
+        : BaseController(configuration, tokenAcquisition, telemetryClient, httpClientFactory)
+    {
+        [Route("{runId}")]
+        public async Task<IActionResult> Index(int runId)
+        {
+            var runDetails = await this.GetCalculatorRundetails(runId);
+            if (!this.ModelState.IsValid) { }
+
+            var viewModel = new ReasonForRejectionViewModel()
+            {
+                CalculationRunId = runDetails.RunId,
+                CalculationRunName = runDetails.RunName,
+                Reason = string.Empty,
+                BackLink = ControllerNames.BillingInstructionsController,
+            };
+
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        [ActionName("Index")]
+        [Route("{runId}")]
+        public IActionResult IndexPost(int runId, ReasonForRejectionViewModel model)
+        {
+            if (!this.ModelState.IsValid)
+            {
+                return this.View(model);
+            }
+
+            return this.RedirectToAction(ActionNames.Index, new { model.CalculationRunId });
+        }
+    }
+}
---
+++ src/EPR.Calculator.Frontend/ViewModels/ReasonForRejectionViewModel.cs
@@ -0,0 +1,16 @@
+﻿using Pipelines.Sockets.Unofficial.Arenas;
+using System.ComponentModel.DataAnnotations;
+using System.Drawing;
+
+namespace EPR.Calculator.Frontend.ViewModels
+{
+    public record ReasonForRejectionViewModel : ViewModelCommonData
+    {
+        public int CalculationRunId { get; set; }
+
+        public string? CalculationRunName { get; set; }
+
+        [Required(ErrorMessage = "Provide a reason that applies to all the billing instructions you selected for rejection.")]
+        public required string Reason { get; set; }
+    }
+}
