98388dc4a4f91541c8ce312df35410d7b7489a9f
Jagdip Singh (Atos) <jagdip.singh.external@eviden.com>
2025-07-15 11:31:50 +0100

    Feature/583331 Setup Service (Pre-Integration) HttpPut ProducerBillingInstructions (#264)
    * Accept/Reject : Confirm billing instructions Screen - Add header and content
    
    * Change for SonarQube
    
    * SonarQube
    
    * Removed trailing space
    
    * SonarQube
    
    * SonarQube
    
    * SonarQube
    
    * SonarQube
    
    * SonarQube
    
    * Code review
    
    * include stylesheet
    
    * SonarQube
    
    * Initial checkin for Select All records
    
    * removed blank space
    
    * refactored code
    
    * fix for test
    
    * Update the code to get data from GetBillingData
    
    * Refactored the code
    
    * Fixed SQ fail
    
    * Implement select all on page
    
    * fix merge issues
    
    * format code
    
    * refactored and added unit tests
    
    * update on select records
    
    * SQ fix
    
    * add selectedid's to session
    
    * SQ fail fix
    
    * Unit test fail fix
    
    * Sonar fix
    
    * add unit tests and updated the selection number on UI
    
    * update the selected count
    
    * sonar issue fixed
    
    * sonar issue fix
    
    * billing instructions first pass without unit tests
    
    * add unit test
    
    * format code
    
    * update unit test
    
    * sonar issue fix
    
    * refactored code
    
    * format fix
    
    * Refactored code
    
    * Pagination
    
    * small update
    
    * implemented the select all page
    
    * implement select all page
    
    * Unit tests fix
    
    * unit test fail fix
    
    * add unit tests
    
    * sonar issue fix
    
    * Add unit tests.
    
    * Sonar Qube Coverage fix
    
    * removed space
    
    * SQ fix
    
    * SOnar fixes
    
    * added few tesets for SessionExtensions
    
    * update unit tests
    
    * fix merge issue
    
    * update unit test
    
    * fix selected count
    
    * Addressed review comments
    
    * update logic to maintain state for select all
    
    * fix sonar issue
    
    * fix sonar issue
    
    * add unit tests
    
    * Add unit test
    
    * fix sonar issue
    
    * fix sonar issues
    
    * fix ui issues and sonar
    
    * fix sonar issue
    
    * sonar issue fix
    
    * format fix
    
    * accessibility issue fix
    
    * updated condition on pagination and select all
    
    * fix sonar issue
    
    * refactored code
    
    * Feature/582145 Accept/Reject CheckBox single selection adding to Session (#262)
    
    * first pass
    
    * First pass
    
    * Reconcile session changes and make readability refactors
    
    * fix blank line
    
    * Add ARJourneySessionHelper and switchover on BillingInstructionsController
    
    * sonar fix
    
    * fix sonar style issue
    
    * constant fields
    
    * Remove trailing 0s
    
    * Remove more trailing whitespaces
    
    * Add test coverage for the new changes
    
    ---------
    
    Co-authored-by: Uday Denduluri <uday.2.denduluri.external@eviden.com>
    
    * initial draft w/o units
    
    * fix sonar related issues
    
    * Fix sonar issues
    
    * asdf
    
    * Update test coverage
    
    * Fix using order
    
    ---------
    
    Co-authored-by: Shilpi Goel <Shilpi.Goel@goaco.com>
    Co-authored-by: Mazar Shaik <mazar.shaik.external@eviden.com>
    Co-authored-by: Santhosh kumar <111443660+Skumarhpa@users.noreply.github.com>
    Co-authored-by: Uday Denduluri <uday.2.denduluri.external@eviden.com>

--- src/EPR.Calculator.Frontend/Program.cs
+++ src/EPR.Calculator.Frontend/Program.cs
@@ -1,8 +1,8 @@
-﻿using System.Reflection;
-using EPR.Calculator.Frontend.Constants;
+﻿using EPR.Calculator.Frontend.Constants;
 using EPR.Calculator.Frontend.Exceptions;
 using EPR.Calculator.Frontend.HealthCheck;
 using EPR.Calculator.Frontend.Mappers;
+using EPR.Calculator.Frontend.Services;
 using EPR.Calculator.Frontend.Validators;
 using FluentValidation;
 using FluentValidation.AspNetCore;
@@ -12,6 +12,7 @@ using Microsoft.AspNetCore.Mvc.Authorization;
 using Microsoft.FeatureManagement;
 using Microsoft.Identity.Web;
 using Microsoft.Identity.Web.UI;
+using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 var environmentName = builder.Environment.EnvironmentName?.ToLower() ?? string.Empty;
@@ -65,6 +66,8 @@ builder.Services.AddHttpClient();
 builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
 
 builder.Services.AddScoped<IBillingInstructionsMapper, BillingInstructionsMapper>();
+
+builder.Services.AddScoped<IBillingInstructionsApiService, BillingInstructionsApiService>();
 
 builder.Configuration.AddUserSecrets(Assembly.GetExecutingAssembly(), true);
 
