7fc9eede4b0f9a12d2fd00dd6cfa6d67bc4c8900
atosjag <jagdip.singh.external@atos.net>
2025-07-21 15:56:59 +0100

    Add downloadfile controller

---
+++ src/EPR.Calculator.Frontend/Controllers/DownloadFileController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.ApplicationInsights;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Identity.Web;
+
+namespace EPR.Calculator.Frontend.Controllers
+{
+    /// <summary>
+    /// Controller for handling result/billing file downloads.
+    /// </summary>
+    [Route("[controller]")]
+    public class DownloadFileController(
+        IConfiguration configuration,
+        ITokenAcquisition tokenAcquisition,
+        TelemetryClient telemetryClient,
+        IHttpClientFactory httpClientFactory)
+        : BaseController(configuration, tokenAcquisition, telemetryClient, httpClientFactory)
+    {
+        /// <summary>
+        /// Download file entry point.
+        /// </summary>
+        /// <param name="runId">The ID of the calculation run.</param>
+        /// <returns>A results or download file.</returns>
+        [HttpGet]
+        [Route("{runId}")]
+        public async Task<IActionResult> Index(int runId)
+        {
+            throw new NotImplementedException("This method is not implemented yet. Please use PostBillingFileController instead.");
+        }
+    }
+}
