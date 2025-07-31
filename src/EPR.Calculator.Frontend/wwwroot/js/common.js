6cb668712cd2525a72238773470832681e524cec
hamzahshakeel907 <hamzah.shakeel.external@eviden.com>
2025-07-31 13:52:18 +0100

    removed files

--- src/EPR.Calculator.Frontend/wwwroot/js/common.js
+++
@@ -1,7 +0,0 @@
-    function preventFormReSubmission(event, formElement) {
-        if (formElement.dataset.submitted) {
-            event.preventDefault();
-            return;
-        }
-        formElement.dataset.submitted = true;       
-    }
