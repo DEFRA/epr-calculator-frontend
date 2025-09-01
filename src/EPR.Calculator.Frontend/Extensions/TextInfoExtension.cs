2d65ea8c704be3de399e2b899ddb0e81a5061b14
msgeviden <manraj.singh.external@atos.net>
2025-09-01 12:00:44 +0100

    Feature/581840 dynamic screen post initialrun classification (#292) (#315)
    * Feature/581840 dynamic screen post initialrun classification (#292)
    
    * Added business logic for showing important information based on choices available for classifying the calculator run
    
    * updated ordering of enum of  final-recalculation completed and final-run completed, updated importan messages text for different classify status
    
    * fixed typo "RundId" to "RunId" in query param
    
    * updated info to get if final or final recalc already ran
    
    * made propety nullable
    
    * Updated important calculator run logic information text as found in PR review
    
    * fixed sonar qube issue, deference a possible null
    
    * fixed sonar qube issue
    
    * Added unit tests for set classification status description (which was missing from earlier code) and information
    
    * Code review, remove order by status, get default ordering from Api
    
    * merge back post initlal branch and fixed  unit tests
    
    * removed unused config properties
    
    * fixed dereference of possible null reference
    
    * fixed null reference check
    
    * Fix for select all with filter (#298)
    
    * Feature/fix sonar issues in main (#301)
    
    * Sonar Issues
    
    * SonarQube Issue
    
    * SonarQube Issues
    
    ---------
    
    Co-authored-by: Shilpi Goel <Shilpi.Goel@goaco.com>
    
    * Feature/fix sonar issues in main (#302)
    
    * Sonar Issues
    
    * SonarQube Issue
    
    * SonarQube Issues
    
    * Removed duplicate selecetors
    
    ---------
    
    Co-authored-by: Shilpi Goel <Shilpi.Goel@goaco.com>
    
    * Fix ammend button (#300)
    
    Co-authored-by: Uday Denduluri <uday.2.denduluri.external@atos.net>
    
    * resolve conflicts
    
    * Fixes the text issues
    
    * fix sonar issue
    
    * fix sonar issues
    
    ---------
    
    Co-authored-by: Mazar-Shaik <mazar.shaik.external@eviden.com>
    Co-authored-by: Shilpi Goel <shilpi.goel.external@eviden.com>
    Co-authored-by: Shilpi Goel <Shilpi.Goel@goaco.com>
    Co-authored-by: David Jayakumar <david.2.jayakumar.external@eviden.com>
    Co-authored-by: Uday Denduluri <uday.2.denduluri.external@atos.net>
    Co-authored-by: Rekha Devi <rekha.devi.external@atos.net>
    
    * add content security policy (#309)
    
    * add content security policy
    
    * refactor code
    
    * update the header
    
    * update the headers for CSP and click jack
    
    * update the csp headers (#313)
    
    * Fix issue Strict-Transport-Security Header Not Set (#310)
    
    * Fix issue Strict-Transport-Security Header Not Set
    
    * Remove comment
    
    * Remove comment
    
    * Feature/599210 rel14 dev8 paycal zap security scan cookie without same site attribute  (#312)
    
    * Removed duplicate css selectors
    
    * ZAP Security scan - Cookie without SameSite Attribute
    
    * ZAP issue for Cookie
    
    ---------
    
    Co-authored-by: Shilpi Goel <Shilpi.Goel@goaco.com>
    Co-authored-by: Uday Denduluri <uday.2.denduluri.external@atos.net>
    
    ---------
    
    Co-authored-by: Jignesh Jethwani <jignesh.jethwani.external@atos.net>
    Co-authored-by: Mazar-Shaik <mazar.shaik.external@eviden.com>
    Co-authored-by: Shilpi Goel <shilpi.goel.external@eviden.com>
    Co-authored-by: Shilpi Goel <Shilpi.Goel@goaco.com>
    Co-authored-by: David Jayakumar <david.2.jayakumar.external@eviden.com>
    Co-authored-by: Uday Denduluri <uday.2.denduluri.external@atos.net>
    Co-authored-by: Rekha Devi <rekha.devi.external@atos.net>
    Co-authored-by: Santhosh kumar <111443660+Skumarhpa@users.noreply.github.com>
    Co-authored-by: rekhadevieviden <rekha.devi.external@eviden.com>

---
+++ src/EPR.Calculator.Frontend/Extensions/TextInfoExtension.cs
@@ -0,0 +1,20 @@
+﻿using System.Globalization;
+
+namespace EPR.Calculator.Frontend.Extensions
+{
+    public static class TextInfoExtension
+    {
+        public static string ToFirstLetterCap(this TextInfo textInfo, string text)
+        {
+            if (string.IsNullOrWhiteSpace(text))
+            {
+                return text;
+            }
+
+            string lowercaseText = textInfo.ToLower(text);
+            char firstChar = textInfo.ToUpper(lowercaseText[0].ToString())[0];
+
+            return firstChar + lowercaseText.Substring(1);
+        }
+    }
+}
