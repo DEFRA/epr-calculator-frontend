c690b3e8509526cf328ad5843050dfbf95971731
hamzahshakeel907 <hamzah.shakeel.external@eviden.com>
2025-09-04 16:17:41 +0100

    adding exclude

--- src/EPR.Calculator.Frontend/ViewModels/BackLinkPartialViewModel.cs
+++ src/EPR.Calculator.Frontend/ViewModels/BackLinkPartialViewModel.cs
@@ -1,5 +1,8 @@
-﻿namespace EPR.Calculator.Frontend.ViewModels
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace EPR.Calculator.Frontend.ViewModels
 {
+    [ExcludeFromCodeCoverage]
     public record BackLinkPartialViewModel
     {
         public string BackLink { get; set; } = null!;
