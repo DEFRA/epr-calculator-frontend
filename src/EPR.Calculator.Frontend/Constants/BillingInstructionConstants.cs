6f3b796d9afde9108648c6857696a1c1687b7a8a
Mazar Shaik <mazar.shaik.external@eviden.com>
2025-09-16 12:13:44 +0100

    space fix for test fail

--- src/EPR.Calculator.Frontend/Constants/BillingInstructionConstants.cs
+++ src/EPR.Calculator.Frontend/Constants/BillingInstructionConstants.cs
@@ -33,6 +33,5 @@
         public const string BillingFileOutofDateBanner = "The billing instructions have changed, so the draft billing file is now out of date. You must first {0} before it can be sent to the FSS.";
 
         public const string GenerateBillingFile = "generate a new draft billing file";
-
     }
 }
