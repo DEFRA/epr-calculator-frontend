aa3a3045505f8666ef728e21c0213bdc5b4871b1
Hamzah Shakeel <hamzah.shakeel.external@atos.net>
2025-09-26 14:06:53 +0100

    SonarQube cleanup of code smells (#317)
    * SQ Fixes
    
    * final changes
    
    * try this
    
    * adding exclude
    
    ---------
    
    Co-authored-by: PaulCrippsEviden <paul.cripps.external@eviden.com>

--- src/EPR.Calculator.Frontend/Models/CalculatorRunClassificationDto.cs
+++ src/EPR.Calculator.Frontend/Models/CalculatorRunClassificationDto.cs
@@ -1,11 +1,14 @@
-﻿namespace EPR.Calculator.Frontend.Models
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace EPR.Calculator.Frontend.Models
 {
+    [ExcludeFromCodeCoverage]
     public class CalculatorRunClassificationDto
     {
         public int Id { get; set; }
 
-        public string Status { get; set; }
+        public string Status { get; set; } = null!;
 
-        public string Description { get; set; }
+        public string Description { get; set; } = null!;
     }
 }
