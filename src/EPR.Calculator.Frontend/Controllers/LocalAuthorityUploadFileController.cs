a18e6dcff60ff60aa38e72987370ec8398f58a61
Mike McBain <mike.mcbain@esynergy.co.uk>
2026-09-10 18:09:58 +0100

    ECV-700: Refactors Local Authority Disposal Costs upload process

--- src/EPR.Calculator.Frontend/Controllers/LocalAuthorityUploadFileController.cs
+++ src/EPR.Calculator.Frontend/Controllers/LocalAuthorityUploadFileController.cs
@@ -1,85 +1,75 @@
-using System.Net;
 using EPR.Calculator.Frontend.Helpers;
-using EPR.Calculator.Frontend.Helpers.Csv;
+using EPR.Calculator.Frontend.Helpers.Csv.Lapcap;
 using EPR.Calculator.Frontend.Models;
 using EPR.Calculator.Frontend.Services;
-using EPR.Calculator.Frontend.ViewModels;
+using EPR.Calculator.Frontend.ViewModels.CsvUpload;
 using Microsoft.AspNetCore.Mvc;
 
 namespace EPR.Calculator.Frontend.Controllers;
 
 public class LocalAuthorityUploadFileController(
     IConfiguration configuration,
-    IEprCalculatorApiService eprCalculatorApiService,
-    ILogger<LocalAuthorityUploadFileController> logger
+    IEprCalculatorApiService eprCalculatorApiService
 ) : BaseController
 {
     private const string ApiErrorsKey = "Local_Authority_Upload_Errors";
 
-    private static readonly FileUploadErrorViewModel ErrorTemplate = new()
+    private CsvUploadViewModel UploadTemplate => new()
     {
-        InputId = LapcapUploadViewModel.DomElements.InputId,
-        DetailsId = LapcapUploadViewModel.DomElements.ErrorDetailsId,
-        CallToActionId = LapcapUploadViewModel.DomElements.ErrorCallToActionId,
-        FileErrors = [],
-        ContentErrors = []
+        Title = "Upload new local authority disposal costs",
+        BackLinkUrl = Url.Action("Index", "LocalAuthorityDisposalCosts")!,
+        UploadUrl = Url.Action("Upload", "LocalAuthorityUploadFile")!
     };
 
     [HttpGet]
     public IActionResult Index()
     {
-        return View("Index", new LapcapUploadViewModel());
+        return View("Views/CsvUpload/Index", UploadTemplate);
     }
 
     [HttpPost]
     public async Task<IActionResult> Upload(IFormFile? fileUpload, CancellationToken cancellationToken)
     {
-        try
+        var result = await LapcapCsvFileHelper.Parse(fileUpload, cancellationToken);
+
+        if (!result.IsSuccess)
         {
-            var result = await LapcapCsvFileHelper.Parse(fileUpload, cancellationToken);
-
-            if (!result.IsSuccess)
+            return View("Views/CsvUpload/Index", UploadTemplate with
             {
-                return View("Index", new LapcapUploadViewModel
+                Errors = new CsvUploadViewModel.ErrorsViewModel
                 {
-                    ErrorsViewModel = ErrorTemplate with
-                    {
-                        FileErrors = result.FileErrors,
-                        ContentErrors = result.ContentErrors
-                    }
-                });
-            }
-
-            return View("Processing", new LapcapProcessingViewModel
-            {
-                Filename = fileUpload!.FileName,
-                Values = result.Records
+                    FileErrors = result.FileErrors,
+                    ContentErrors = result.ContentErrors
+                }
             });
         }
-        catch (Exception ex)
+
+        var processRequest = new SetLapcapDataRequest
         {
-            logger.LogError(ex, "Uncaught exception when handling CSV file upload");
-            return RedirectToError();
-        }
+            Filename = fileUpload!.FileName,
+            RelativeYear =  CommonUtil.GetRelativeYear(HttpContext.Session, CommonUtil.GetRelativeYearStartingMonth(configuration)),
+            Values = result.Records
+        };
+
+        return View("Views/CsvUpload/Processing", new CsvUploadProcessingViewModel
+        {
+            ProcessingUrl = Url.Action("Process", "LocalAuthorityUploadFile")!,
+            SuccessUrl = Url.Action("Index", "LocalAuthorityConfirmation")!,
+            ErrorUrl = Url.Action("Errors", "LocalAuthorityUploadFile")!,
+            Payload = processRequest
+        });
     }
 
     [HttpPost]
-    public async Task<IActionResult> Process([FromBody] LapcapProcessingViewModel model, CancellationToken cancellationToken)
+    public async Task<IActionResult> Process([FromBody] SetLapcapDataRequest request, CancellationToken cancellationToken)
     {
-        var request = new CreateLapcapDataRequest
-        {
-            Filename = model.Filename,
-            RelativeYear =  CommonUtil.GetRelativeYear(HttpContext.Session, CommonUtil.GetRelativeYearStartingMonth(configuration)),
-            Values = model.Values
-        };
-
         using var response = await eprCalculatorApiService.CallApi(
-            HttpMethod.Post,
+            HttpMethod.Put,
             "v1/lapcapData",
             body: request,
             cancellationToken: cancellationToken);
 
-        if (response is { IsSuccessStatusCode: true, StatusCode: HttpStatusCode.Created })
+        if (response is { IsSuccessStatusCode: true })
             return NoContent();
 
         var errorContent = await response.Content.ReadAsStringAsync(cancellationToken);
@@ -98,11 +88,12 @@ public class LocalAuthorityUploadFileController(
             return RedirectToError();
 
         // The API validates the contents of the file, so anything it rejects is a content error.
-        return View("Index", new LapcapUploadViewModel
+        return View("Views/CsvUpload/Index", UploadTemplate with
         {
-            ErrorsViewModel = ErrorTemplate with
+            Errors = new CsvUploadViewModel.ErrorsViewModel
             {
-                ContentErrors = [..problemDetails.Errors.SelectMany(kv => kv.Value)]
+                FileErrors = [],
+                ContentErrors = [.. problemDetails.Errors.SelectMany(kv => kv.Value)]
             }
         });
     }
